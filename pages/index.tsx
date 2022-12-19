137a932ecc1a4ddd74cb2075b0289c0b6156c392
Avikalp Kumar Gupta <avikalpgupta@gmail.com>
2022-12-19 11:01:37 +0530

    tailwind setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,14 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
 	return (
-		<div className={styles.container}>
+		<div className='flex min-h-screen flex-col items-center justify-center py-2'>
 			<Head>
 				<title>DevProfile: The Ultimate Developer Profile</title>
 				<meta name="description" content="Build your developer profile directly from your code's version history" />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
-			<h1>Developer Profile</h1>
+			<h1 className='text-3xl font-bold underline'>Developer Profile</h1>
 			<footer>Alokit Innovations Private Limited</footer>
 		</div>
 	)
