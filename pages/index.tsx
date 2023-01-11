a15bdabf4d7efb701547eed9200779ac1195c09d
MuskanPaliwal <muskan10112002@gmail.com>
2023-01-11 17:18:30 +0530

    used `uuid` npm package to create anonymousId for first time users

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,10 +5,14 @@ import Steps from '../views/Steps'
 import Hero from '../views/Hero'
 import WhyUs from '../views/WhyUs'
 import { rudderstack_initialize } from "./rudderstack_initialize";
+import { v4 as uuidv4} from 'uuid';
 
 export default function Home() {
+
+	let anonymousId = uuidv4();
 	React.useEffect(() => {
 		rudderstack_initialize();
+		localStorage.setItem('AnonymousId', anonymousId);
 	  }, []);
   return (
     <div>
