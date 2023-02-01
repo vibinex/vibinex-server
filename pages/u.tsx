9ee838fbb9a2fc6cc016217caae35d910774f064
Avikalp Kumar Gupta <avikalpgupta@gmail.com>
2023-02-02 00:09:57 +0530

    PostGreSQL integration: prisma ORM installed and POC written

--- pages/u.tsx
+++ pages/u.tsx
@@ -1,15 +1,19 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import MainAppBar from "../views/MainAppBar";
+import { PrismaClient } from '@prisma/client'
+import { NextPageContext } from "next";
 
-export default function Profile() {
+export default function Profile(data: { repo_data: object | null }) {
 	const router = useRouter();
 	const [isLoggedIn, setIsLoggedIn] = useState(false);
 	useEffect(() => {
 		// check if devProfile already exists. Redirect to upload page if it doesn't
-		window.location.href = "/upload"
+		if (false)
+			window.location.href = "/upload"
 	}, [])
 	useEffect(() => {
+		// Setting user information in localStorage immediately after login
 		if ((Object.keys(router.query).length != 0) && ('name' in router.query)) {
 			if (router.query.name && typeof router.query.name === "string") {
 				localStorage.setItem('name', router.query.name);
@@ -24,6 +28,27 @@ export default function Profile() {
 		<>
 			<MainAppBar isLoggedIn={isLoggedIn} />
 			<p>This is the developer profile</p>
+			<p>{JSON.stringify(data.repo_data)}</p>
 		</>
 	)
+}
+
+Profile.getInitialProps = async (ctx: NextPageContext) => {
+	const my_repo = "mentorship-website";
+	const prisma = new PrismaClient();
+	const result: object | null = await prisma.temp_devraw.groupBy({
+		by: ['author_email'],
+		_count: {
+			commit_id: true,
+		},
+		where: {
+			commit_json: {
+				path: ['repo_name'],
+				equals: my_repo,
+			}
+		}
+	});
+	return {
+		repo_data: result
+	}
 }
