dd0f994807c278d3d9eb2ea8f1e0077028756495
ajayVibinex <ajay.yadav@vibinex.com>
2023-02-25 13:29:40 +0530

    removed legends

--- pages/repoprofile.tsx
+++ pages/repoprofile.tsx
@@ -5,32 +5,32 @@ const barData = [
     {
         "fileName": "main.ts",
         "commits": 172,
-        "profile.tsColor": "hsl(20, 70%, 50%)",
+        "main.tsColor": "hsl(20, 70%, 50%)",
     },
     {
         "fileName": "index.ts",
         "commits": 151,
-        "main.tsColor": "hsl(193, 70%, 50%)",
+        "index.tsColor": "hsl(193, 70%, 50%)",
     },
     {
         "fileName": "Heros.ts",
         "commits": 145,
-        "index.tsColor": "hsl(39, 70%, 50%)"
+        "Heros.tsColor": "hsl(39, 70%, 50%)"
     },
     {
         "fileName": "profile.ts ",
         "commits": 187,
-        "login.tsColor": "hsl(163, 70%, 50%)",
+        "profile.tsColor": "hsl(163, 70%, 50%)",
     },
     {
         "fileName": "setting.ts",
         "commits": 165,
-        "profile.tsColor": "hsl(285, 70%, 50%)",
+        "setting.tsColor": "hsl(285, 70%, 50%)",
     },
     {
         "fileName": "login.ts",
         "commits": 116,
-        "main.tsColor": "hsl(112, 70%, 50%)",
+        "login.tsColor": "hsl(112, 70%, 50%)",
     }
 ]
 
@@ -39,13 +39,13 @@ const MyResponsiveRadar = () => {
         <div className='h-[50rem] w-[90%] m-auto'>
             <div className='border-2 mt-10 p-2 rounded-md border-blue-200 text-[20px]'>
             <h3> <span>Repo Name : </span>Repo Profile Overview</h3>
-            <h1><span>Own by : </span>Vibinex</h1>
+            <h1><span>Owned by : </span>Vibinex</h1>
             </div>
 
             <div className='h-[35rem] w-[100%] m-auto border-2 mt-5 p-2 pb-12 rounded-md border-blue-200'>
                 <div className='ml-5 mt-5 mb-[-10px]'>
                 <h3>Metric : File Contribution</h3>
-                <h4 className='text-gray-500'>About : Used to find out the total number of files used in repo. You can see language used, code chunk, and much more details of each file.</h4>
+                <h4 className='text-gray-500'>About : Used to find out the total number of commits in each file.</h4>
                 </div>
             <ResponsiveBar
                 data={barData}
@@ -115,7 +115,7 @@ const MyResponsiveRadar = () => {
                     tickSize: 5,
                     tickPadding: 5,
                     tickRotation: 0,
-                    legend: 'Number of lines',
+                    legend: 'Number of commits',
                     legendPosition: 'middle',
                     legendOffset: -40
                 }}
@@ -130,35 +130,12 @@ const MyResponsiveRadar = () => {
                         ]
                     ]
                 }}
-                legends={[
-                    {
-                        dataFrom: 'keys',
-                        anchor: 'bottom-right',
-                        direction: 'column',
-                        justify: false,
-                        translateX: 120,
-                        translateY: 0,
-                        itemsSpacing: 2,
-                        itemWidth: 100,
-                        itemHeight: 20,
-                        itemDirection: 'left-to-right',
-                        itemOpacity: 0.85,
-                        symbolSize: 20,
-                        effects: [
-                            {
-                                on: 'hover',
-                                style: {
-                                    itemOpacity: 1
-                                }
-                            } 
-                        ]
-                    }
-                ]}
+              
                 role="application"
                 ariaLabel="Nivo bar chart demo"
                 barAriaLabel={function (e) { return e.id + ": " + e.formattedValue + " in fileName: " + e.indexValue }}
             />
-</div>
+            </div>
         </div>
     )
 }
