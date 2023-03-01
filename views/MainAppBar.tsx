d1e3f79ae8f8f34f8ff848463457d67a9d7be531
Avikalp Kumar Gupta <avikalpgupta@gmail.com>
2023-03-01 16:50:38 +0530

    Changed the Logo

--- views/MainAppBar.tsx
+++ views/MainAppBar.tsx
@@ -23,8 +23,7 @@ export default function MainAppBar({ isLoggedIn }: { isLoggedIn?: boolean }) {
 	return (
 		<AppBar position='fixed' className='w-full flex flex-row'>
 			<a href="/" className="mr-16 h-full overflow-clip flex flex-row items-center">
-				<img src="/DevProfile.Tech Logo v1.2.png" alt="Dev Profile Logo" className="h-full w-auto mr-2" />
-				<h1 className='w-auto text-2xl text-white'>devProfile</h1>
+				<img src="/black-logo.svg" alt="Dev Profile Logo" className="h-full w-auto mr-2" />
 			</a>
 			<span className='flex-grow'></span>
 			<Button variant='text' href='https://github.com/Alokit-Innovations/dev-profile-website' target='_blank' className='text-white'>Contribute</Button>
