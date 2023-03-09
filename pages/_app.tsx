af4be9762662d11f7b791e501c1489f1bdbf414b
Avikalp Kumar Gupta <avikalpgupta@gmail.com>
2023-03-09 13:01:55 +0530

    added next-auth secret and made requested changes from PR

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,11 +7,12 @@ export default function App({ Component, pageProps: { session, ...pageProps } }:
   const keywords = [
     "git", "GitHub", "BitBucket", "GitLab",
     "pull requests", "review", "code review", "pull request review", "PR review", "code quality", "quality control",
-    "vibinex", "Vibinex", "SonarQube", "DeepSource", "Sonar Cloud", "DeepSource alternative",
+    "context for PR review", "prioritization among pull requests", "review coverage of pull request",
+    "vibinex", "Vibinex", "SonarQube", "DeepSource", "Sonar Cloud", "DeepSource alternative", "SonarQube alternative", "Sonar Cloud alternative",
     "devtool", "developer", "productivity", "software development", "software engineer", "developer productivity", "SDE productivity",
     "Engineering managers", "best tools for EMs", "best tools for tech leads",
     "QA tool", "QA tools for engineering managers", "QA tool for tech leads",
-    "Chrome extension", "Chrome extension   for PR review"
+    "Chrome extension", "Chrome extension for PR review", "Slackbot for PR reviews"
   ]
   return (
     <SessionProvider session={session}>
