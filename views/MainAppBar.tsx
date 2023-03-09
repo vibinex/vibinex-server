5684860a1b0421ce06434e55d76239122139281d
ajayVibinex <ajay.yadav@vibinex.com>
2023-03-09 11:43:23 +0530

    changes navBar ui to pop up signout button on profile pic click

--- views/MainAppBar.tsx
+++ views/MainAppBar.tsx
@@ -1,33 +1,54 @@
-import AppBar from '../components/AppBar'
-import Button from '../components/Button'
+import { useState } from "react";
 import Image from "next/image";
-import { useState, useEffect } from "react";
 import { signOut } from "next-auth/react"
+import Link from 'next/link';
+import Avatar from '../public/avatar/icon2.jpg';
 
 export default function MainAppBar({ isLoggedIn }: { isLoggedIn?: boolean }) {
-	const [name, setName] = useState('');
-	const [profilePic, setProfilePic] = useState("/../public/dummy-profile-pic-female-300n300.jpeg");
 
-	useEffect(() => {
-		if (localStorage.getItem("name") && localStorage.getItem('displayPic')) {
-			const name_var = localStorage.getItem("name");
-			const profilePic_var = localStorage.getItem('displayPic');
-			setName(name_var != null ? name_var : name);
-			setProfilePic(profilePic_var ? profilePic_var : profilePic);
-			isLoggedIn = true;
-		}
-		// FIXME: If the user is not logged in, redirect from here, if there is no middleware to handle that
-		// TODO: If the user is logged in, check if they already have a profile. Redirect to /u if they do
-	}, [isLoggedIn]);
+	const [isOpen, setIsOpen] = useState(false);
 
 	return (
-		<AppBar position='fixed' className='w-full flex flex-row'>
-			<a href="/" className="mr-16 h-full overflow-clip flex flex-row items-center">
-				<img src="/black-logo.svg" alt="Dev Profile Logo" className="h-full w-auto mr-2" />
-			</a>
-			<span className='flex-grow'></span>
-			<Button variant='text' href='https://github.com/Alokit-Innovations/dev-profile-website' target='_blank' className='text-white'>Contribute</Button>
-			<Image src={profilePic} onClick={() => signOut()} alt="Display picture" title={name} width={300} height={300} className="h-full w-auto hover:cursor-pointer" />
-		</AppBar>
-	)
-}
+		<div
+			className={
+				'fixed left-0 top-0 w-full z-10 ease-in duration-300 border-b-secondary-dark border-b-2  bg-primary-light'
+			}
+		>
+
+			{/* // Navbar Content */}
+			<div className={'max-w-[1240px] m-auto flex justify-between items-center p-4 text-secondary-dark'
+			}>
+				<Link href='/'>
+					<h1 className='font-bold text-4xl'>
+						Vibinex
+					</h1>
+				</Link>
+				<div onClick={() => setIsOpen(prev => !prev)} className='cursor-pointer hidden sm:block'>
+					<Image src={Avatar} className='w-[2%] h-[50%] rounded-full absolute top-5 object-contain' alt='user profile pic' />
+				</div>
+
+				{/* Mobile Button */}
+				<div
+					className={
+						'block sm:hidden z-10 text-secondary-dark'
+					}
+				>
+					<div onClick={() => setIsOpen(prev => !prev)} className='cursor-pointer'>
+						<Image src={Avatar} className='w-[10%] h-[50%] rounded-full absolute top-5 object-contain right-3' alt='user profile pic' />
+					</div>
+				</div>
+			</div>
+
+			{/* Log out Pop up  */}
+			{isOpen ?
+				<div className='w-[40%] sm:w-[15%] p-3 rounded-md absolute  right-5 sm:right-10 top-20 border-2 bg-primary-light'>
+					<div className='bg-primary-main p-2 text-center rounded-md cursor-pointer' onClick={() => signOut()}>
+						<h2 className='text-primary-light'>Logout</h2>
+					</div>
+				</div>
+				:
+				null
+			}
+		</div>
+	);
+};
