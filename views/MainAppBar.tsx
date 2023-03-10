d1a4fe9211085eb97e812d3f1b9bbe4c73cbe9b0
ajayVibinex <ajay.yadav@vibinex.com>
2023-03-10 15:43:07 +0530

    reuse Appbar and use semantic tags.

--- views/MainAppBar.tsx
+++ views/MainAppBar.tsx
@@ -1,54 +1,45 @@
-import { useState } from "react";
+import AppBar from '../components/AppBar'
+import Button from '../components/Button'
 import Image from "next/image";
+import { useState, useEffect } from "react";
 import { signOut } from "next-auth/react"
-import Link from 'next/link';
-import Avatar from '../public/dummy-profile-pic-female-300n300.jpeg';
 
 export default function MainAppBar({ isLoggedIn }: { isLoggedIn?: boolean }) {
+	const [name, setName] = useState('');
+	const [profilePic, setProfilePic] = useState("/../public/dummy-profile-pic-female-300n300.jpeg");
+	const [showPop,setShowPop] = useState(false);
 
-	const [isOpen, setIsOpen] = useState(false);
+	useEffect(() => {
+		if (localStorage.getItem("name") && localStorage.getItem('displayPic')) {
+			const name_var = localStorage.getItem("name");
+			const profilePic_var = localStorage.getItem('displayPic');
+			setName(name_var != null ? name_var : name);
+			setProfilePic(profilePic_var ? profilePic_var : profilePic);
+			isLoggedIn = true;
+		}
+		// FIXME: If the user is not logged in, redirect from here, if there is no middleware to handle that
+		// TODO: If the user is logged in, check if they already have a profile. Redirect to /u if they do
+	}, [isLoggedIn]);
 
 	return (
-		<div
-			className={
-				'fixed left-0 top-0 w-full z-10 ease-in duration-300 border-b-secondary-dark border-b-2  bg-primary-light'
-			}
-		>
-
-			{/* // Navbar Content */}
-			<div className={'max-w-[1240px] m-auto flex justify-between items-center p-4 text-secondary-dark'
-			}>
-				<Link href='/'>
-					<h1 className='font-bold text-4xl'>
-						Vibinex
-					</h1>
-				</Link>
-				<div onClick={() => setIsOpen(prev => !prev)} className='cursor-pointer hidden sm:block'>
-					<Image src={Avatar} className='w-[2%] h-[50%] rounded-full absolute top-5 object-contain' alt='user profile pic' />
-				</div>
-
-				{/* Mobile Button */}
-				<div
-					className={
-						'block sm:hidden z-10 text-secondary-dark'
-					}
-				>
-					<div onClick={() => setIsOpen(prev => !prev)} className='cursor-pointer'>
-						<Image src={Avatar} className='w-[10%] h-[50%] rounded-full absolute top-5 object-contain right-3' alt='user profile pic' />
-					</div>
-				</div>
-			</div>
-
+		<AppBar position='fixed' className='w-full py-2 px-10 flex flex-row bg-primary-light border-b-2 border-b-secondary-dark'>
+			<a href="/" className="mr-16 h-full overflow-clip flex flex-row items-center">
+				<h1 className='font-bold text-3xl sm:text-4xl'>
+					Vibinex
+				</h1>
+			</a>
+			<span className='flex-grow'></span>
+			<Image src={profilePic} onClick={() => setShowPop(prev=>!prev)} alt="Display picture" title={name} width={300} height={300} className="h-full w-auto hover:cursor-pointer rounded-xl cursor-pointer" />
 			{/* Log out Pop up  */}
-			{isOpen ?
-				<div className='w-[40%] sm:w-[15%] p-3 rounded-md absolute  right-5 sm:right-10 top-20 border-2 bg-primary-light'>
-					<div className='bg-primary-main p-2 text-center rounded-md cursor-pointer' onClick={() => signOut()}>
+			{showPop ?
+				<section className='w-[40%] sm:w-[15%] p-3 rounded-md absolute  right-5 sm:right-10 top-16 border-2 bg-primary-light'>
+					<article className='bg-primary-main p-2 text-center rounded-md cursor-pointer' onClick={() => signOut()}>
 						<h2 className='text-primary-light'>Logout</h2>
-					</div>
-				</div>
+					</article>
+				</section>
 				:
 				null
 			}
-		</div>
-	);
+		</AppBar>
+	)
 };
