6d3617ddf6e595fa8d0d3d1254943793fe94f104
ajayVibinex <ajay.yadav@vibinex.com>
2023-03-11 13:14:37 +0530

    rename showpop to showMenu

--- views/MainAppBar.tsx
+++ views/MainAppBar.tsx
@@ -7,7 +7,7 @@ import { signOut } from "next-auth/react"
 export default function MainAppBar({ isLoggedIn }: { isLoggedIn?: boolean }) {
 	const [name, setName] = useState('');
 	const [profilePic, setProfilePic] = useState("/../public/dummy-profile-pic-female-300n300.jpeg");
-	const [showPop,setShowPop] = useState(false);
+	const [showMenu,setShowMenu] = useState(false);
 
 	useEffect(() => {
 		if (localStorage.getItem("name") && localStorage.getItem('displayPic')) {
@@ -29,9 +29,9 @@ export default function MainAppBar({ isLoggedIn }: { isLoggedIn?: boolean }) {
 				</h1>
 			</a>
 			<span className='flex-grow'></span>
-			<Image src={profilePic} onClick={() => setShowPop(prev=>!prev)} alt="Display picture" title={name} width={300} height={300} className="h-full w-auto hover:cursor-pointer rounded-xl cursor-pointer" />
+			<Image src={profilePic} onClick={() => setShowMenu(prev=>!prev)} alt="Display picture" title={name} width={300} height={300} className="h-full w-auto hover:cursor-pointer rounded-xl cursor-pointer" />
 			{/* Log out Pop up  */}
-			{showPop ?
+			{showMenu ?
 				<ol className='w-[40%] sm:w-[15%] p-3 rounded-md absolute  right-5 sm:right-10 top-16 border-2 bg-primary-light'>
 					<li className='bg-primary-main p-2 text-center rounded-md cursor-pointer' onClick={() => signOut()}>
 						<h2 className='text-primary-light'>Logout</h2>
