2a6fab69a8ace449f76328833d7fb5b9b7b54aab
Avikalp Kumar Gupta <avikalpgupta@gmail.com>
2023-03-14 16:48:50 +0530

    If gihub login is available, all alias are fetched an populated in db

--- pages/u.tsx
+++ pages/u.tsx
@@ -1,7 +1,12 @@
+import { GetServerSideProps } from "next";
+import { getServerSession, Session } from "next-auth";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
 import LoadingOverlay from "../components/LoadingOverlay";
 import MainAppBar from "../views/MainAppBar";
+import { authOptions } from "./api/auth/[...nextauth]";
+import { getUserByAlias, updateUser } from "../utils/db/users";
+import axios from "axios";
 
 const Profile = () => {
 	const { data: session, status } = useSession();
@@ -23,6 +28,53 @@ const Profile = () => {
 			</p>
 		</>
 	)
+}
+
+type GithubEmailObj = {
+	email: string,
+	primary: boolean,
+	verified: boolean,
+	visibility: string | null
+}
+
+export const getServerSideProps: GetServerSideProps<{ session: Session | null }> = async ({ req, res }) => {
+	// check if user is logged in
+	const session = await getServerSession(req, res, authOptions);
+	if (!session) {
+		return { props: { session } };
+	}
+	if (session.user && session.user.email) {
+		const userWithAlias = await getUserByAlias(session.user.email)
+		if (!userWithAlias || userWithAlias.length < 1) {
+			console.warn(`[Profile] No user found with this email: ${session.user.email}`);
+			return { props: { session } };
+		}
+		if (userWithAlias.length > 1) {
+			console.warn(`[Profile] Multiple users found with this email: ${session.user.email}. Names: `,
+				userWithAlias.map(u => u.name).join(", "));
+			return { props: { session } };
+		}
+		const user = userWithAlias[0];
+		if (Object.keys(user.auth_info!).includes("github")) {
+			const access_key: string = Object.values(user.auth_info!["github"])[0]['access_token'];
+			axios.get("https://api.github.com/user/emails", {
+				headers: {
+					'Accept': 'application/vnd.github+json',
+					'Authorization': `Bearer ${access_key}`
+				}
+			})
+				.then((response: { data: GithubEmailObj[] }) => {
+					const aliases = response.data.map((emailObj: GithubEmailObj) => emailObj.email);
+					updateUser(user.id!, { aliases: aliases }).catch(err => {
+						console.error(`[Profile] Could not update aliases for user (userId: ${user.id})`, err)
+					})
+				})
+		} else {
+			console.warn("Github provider not present");
+			return { props: { session } };
+		}
+	}
+	return { props: { session } }
 }
 
 export default Profile;
