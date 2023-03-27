99eade77112af09c4c44a23ee1e6b027f67fdd1f
MuskanPaliwal <muskan10112002@gmail.com>
2023-03-27 11:22:25 +0530

    changed redirect_uri for the bitbucket auth

--- pages/bitbucket_authorization.tsx
+++ pages/bitbucket_authorization.tsx
@@ -3,7 +3,7 @@ export default function Auth() {
   const handleAuthorize = () => {
     // Construct the URL to the Bitbucket authorization page
     const baseUrl = 'https://bitbucket.org/site/oauth2/authorize';
-    const redirectUri = 'https://7cf7-2401-4900-1f28-7e61-cafc-b24-2e52-83e.in.ngrok.io/authorise_github_app'; // Change this to the orginal redirecturi
+    const redirectUri = 'https://gcscruncsql-k7jns52mtq-el.a.run.app/authorise_bitbucket_consumer'; // Change this to the orginal redirecturi
     const scopes = 'repository';
     const clientId = process.env.NEXT_PUBLIC_BITBUCKET_OAUTH_CLIENT_ID;
 
