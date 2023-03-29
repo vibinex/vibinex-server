d0c6efaba95b1a65f699022c5975c0c425ef1ee2
MuskanPaliwal <muskan10112002@gmail.com>
2023-03-29 12:36:06 +0530

    minor edits

--- pages/instruction_to_setup.tsx
+++ pages/instruction_to_setup.tsx
@@ -5,7 +5,7 @@ export default function SetupInstructions() {
 
   const handleAuthorize = () => {
     const baseUrl = 'https://bitbucket.org/site/oauth2/authorize';
-    const redirectUri = 'https://gcscruncsql-k7jns52mtq-el.a.run.app/authorise_bitbucket_consumer'; // Change this to the orginal redirecturi
+    const redirectUri = 'https://gcscruncsql-k7jns52mtq-el.a.run.app/authorise_bitbucket_consumer';
     const scopes = 'repository';
     const clientId = process.env.NEXT_PUBLIC_BITBUCKET_OAUTH_CLIENT_ID;
 
