dd3cc703d11f1587b58f8c6ae8374e3963c2712e
doomsday4 <amankhilani.ajmer@gmail.com>
2023-06-15 16:44:17 +0530

    microsoft clarity review

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,6 +20,28 @@ export default function App({ Component, pageProps: { session, ...pageProps } }:
         <title>Vibinex • Open-source pull request personalization for GitHub, GitLab & Bitbucket</title>
         <meta name="description" content="Helps you maintain high code quality, save time and ship faster by streamlining your code review process." />
         <meta name="keywords" content={keywords.join(", ")} />
+
+        {/* Microsoft Clarity Update */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "hjsk9imaek");
+            `
+          }}
+        />
+        {/* <script type="text/javascript">
+          (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "hjsk9imaek");
+        </script> */}
+        {/*Microsoft Clarity Update */}
+
       </Head>
       <Component {...pageProps} />
     </SessionProvider>
