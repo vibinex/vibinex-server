3463251acbed940dbfc623e34ed5c4b0388231be
doomsday4 <amankhilani.ajmer@gmail.com>
2023-06-16 15:05:21 +0530

    env var created

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -29,18 +29,10 @@ export default function App({ Component, pageProps: { session, ...pageProps } }:
                 c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                 t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                 y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-              })(window, document, "clarity", "script", "hjsk9imaek");
+              })(window, document, "clarity", "script", "${process.env.CLARITY_ID}");
             `
           }}
         />
-        {/* <script type="text/javascript">
-          (function(c,l,a,r,i,t,y){
-              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-          })(window, document, "clarity", "script", "hjsk9imaek");
-        </script> */}
-        {/*Microsoft Clarity Update */}
 
       </Head>
       <Component {...pageProps} />
