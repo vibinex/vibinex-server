034a7df6e2ace9216b955a5708a03b36fff61f20
siddarth_pai <63923548+siddarthpai@users.noreply.github.com>
2023-06-21 16:01:11 +0530

    working on event.ts (#91)
    * fixed issue using rudderstack types
    * added `object` and removed whitespaces

--- pages/api/events.ts
+++ pages/api/events.ts
@@ -1,7 +1,10 @@
 import Analytics from "@rudderstack/rudder-sdk-node";
 import { apiObject } from "rudder-sdk-js";
 // we need the batch endpoint of the Rudder server you are running
-const client = new Analytics(process.env.RUDDERSTACK_SERVER_WRITE_KEY!, { dataPlaneUrl: process.env.RUDDERSTACK_SERVER_DATA_PLANE_URL });
+const client = (process.env.NODE_ENV === 'development') ? {
+    identify: (event_properties: object) => console.info(event_properties),
+    track: (event_properties:  object) => console.info(event_properties)
+}: new Analytics(process.env.RUDDERSTACK_SERVER_WRITE_KEY!, { dataPlaneUrl: process.env.RUDDERSTACK_SERVER_DATA_PLANE_URL });
 
 const rudderStackEvents = {
     identify: (userId: string, name: string, email: string, githubId: string, role: string, anonymousId: string) => { // Anonymous Id is set in local storage as soon as a user lands on the webiste.
@@ -33,5 +36,4 @@ const rudderStackEvents = {
         );
     }
 }
-
 export default rudderStackEvents;
