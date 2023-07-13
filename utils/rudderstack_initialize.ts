8153a268d74f7200a88384c492ef68566e1833d1
Avikalp Kumar Gupta <avikalpgupta@gmail.com>
2023-07-13 20:10:44 +0530

    Reverted more unnecessary changes

--- utils/rudderstack_initialize.ts
+++ utils/rudderstack_initialize.ts
@@ -4,18 +4,18 @@ export interface RudderstackClientSideEvents {
 		name: string,
 		email: string,
 		anonymousId: string
-	) => void
+	) => void;
 	track: (
 		userId: string,
 		event: string,
 		properties: { [key: string]: any },
 		anonymousId: string
-	) => void
+	) => void;
 	page: (
 		type: string,
 		pageName: string,
 		properties: { [key: string]: any }
-	) => void
+	) => void;
 }
 
 async function loadRudderAnalytics(): Promise<RudderstackClientSideEvents | null> {
@@ -45,7 +45,7 @@ async function loadRudderAnalytics(): Promise<RudderstackClientSideEvents | null
 
 		const rudderstackClientSideEvents: RudderstackClientSideEvents = {
 			identify: (userId, name, email, anonymousId) => {  // Anonymous Id is set in local storage as soon as a user lands on the webiste.
-				console.log("identify")
+				console.log("identify");
 				rudderAnalytics.identify(
 					userId,
 					{ name: name, email: email },
@@ -59,29 +59,34 @@ async function loadRudderAnalytics(): Promise<RudderstackClientSideEvents | null
 			 * @param {*} properties properties should be a dictionary of properties of the event. It must contain an "eventStatusFlag" which will define the status of a single event. If the flag is 1, event is successful and if it is 0, event is failed.
 			 */
 			track: (userId, event, properties, anonymousId) => {
-				console.log("track")
-				properties["userId"] = userId
-				properties["event_timestamp"] = new Date()
+				console.log("track");
+				properties["userId"] = userId;
+				properties["event_timestamp"] = new Date();
 				rudderAnalytics.track(
 					event,
 					properties,
 					{ anonymousId: anonymousId },
 					() => {
-						console.log('Track event successfully recorded')
+						console.log("Track event successfully recorded");
 					}
 				);
 			},
 			page: (type, pageName, properties) => {
 				//TODO: Rudderstack doesn't take `userId` or `anonymousId` as arguments to page method but then does it makes sense to use this method? Figure out.
-				console.log('page')
-				rudderAnalytics.page(type, pageName, properties, () => {
-					console.log('Page event successfully recorded')
-				})
-			}
-		}
-		return rudderstackClientSideEvents
+				console.log("page");
+				rudderAnalytics.page(
+					type,
+					pageName,
+					properties,
+					() => {
+						console.log('Page event successfully recorded')
+					}
+				);
+			},
+		};
+		return rudderstackClientSideEvents;
 	}
-	return null
+	return null;
 }
 
 export async function rudderEventMethods(): Promise<RudderstackClientSideEvents | null> {
