1e11eae75d04e0c6c08c3a3923acbf6516dcdc22
Avikalp Kumar Gupta <avikalpgupta@gmail.com>
2023-07-14 13:27:17 +0530

    Rudderstack setup: handled the case when rudder-sdk-js is not available on prod.

--- utils/rudderstack_initialize.ts
+++ utils/rudderstack_initialize.ts
@@ -23,70 +23,76 @@ async function loadRudderAnalytics(): Promise<RudderstackClientSideEvents | null
 		return null;
 	}
 
-	if (process.env.NODE_ENV === 'development') { // create dummy methods in development environment
-		console.warn('Running in development mode. Rudder Analytics is not loaded.')
-		const emptyAnalyticsObject: RudderstackClientSideEvents = {
-			identify: (...args) => { console.debug(`[identify] args:`, args) },
-			track: (...args) => { console.debug(`[track] args:`, args) },
-			page: (...args) => { console.debug(`[page] args:`, args) }
-		}
-		return emptyAnalyticsObject
+	const mockAnalyticsObject: RudderstackClientSideEvents = {
+		identify: (...args) => { console.debug(`[identify] args:`, args) },
+		track: (...args) => { console.debug(`[track] args:`, args) },
+		page: (...args) => { console.debug(`[page] args:`, args) }
 	}
 
-	const rudderAnalytics = await import("rudder-sdk-js");
-	rudderAnalytics.load(
-		process.env.NEXT_PUBLIC_RUDDERSTACK_CLIENT_WRITE_KEY!,
-		process.env.NEXT_PUBLIC_RUDDERSTACK_CLIENT_DATAPLANE_URL!,
-		{
-			integrations: { All: true }, // load call options
-		}
-	);
+	if (process.env.NODE_ENV === 'development') { // create dummy methods in development environment
+		console.warn('Running in development mode. Rudder Analytics is not loaded.')
+		return mockAnalyticsObject
+	}
 
-	rudderAnalytics.ready(() => {
-		console.debug("Rudder Analytics is all set!!!");
+	const rudderstackClientSideEvents = await import("rudder-sdk-js").then(rudderAnalytics => {
+		rudderAnalytics.load(
+			process.env.NEXT_PUBLIC_RUDDERSTACK_CLIENT_WRITE_KEY!,
+			process.env.NEXT_PUBLIC_RUDDERSTACK_CLIENT_DATAPLANE_URL!,
+			{
+				integrations: { All: true }, // load call options
+			}
+		);
+
+		rudderAnalytics.ready(() => {
+			console.debug("Rudder Analytics is all set!!!");
+		});
+
+		const rsClientSideEvents: RudderstackClientSideEvents = {
+			identify: (userId, name, email, anonymousId) => {  // Anonymous Id is set in local storage as soon as a user lands on the webiste.
+				console.debug("identify");
+				rudderAnalytics.identify(
+					userId,
+					{ name: name, email: email },
+					{ anonymousId: anonymousId },
+					() => {
+						console.debug("identify event successfully recorded");
+					}
+				);
+			},
+			/**
+			 * @param {*} properties properties should be a dictionary of properties of the event. It must contain an "eventStatusFlag" which will define the status of a single event. If the flag is 1, event is successful and if it is 0, event is failed.
+			 */
+			track: (userId, event, properties, anonymousId) => {
+				console.debug("track");
+				properties["userId"] = userId;
+				properties["event_timestamp"] = new Date();
+				rudderAnalytics.track(
+					event,
+					properties,
+					{ anonymousId: anonymousId },
+					() => {
+						console.debug("Track event successfully recorded");
+					}
+				);
+			},
+			page: (type, pageName, properties) => {
+				//TODO: Rudderstack doesn't take `userId` or `anonymousId` as arguments to page method but then does it makes sense to use this method? Figure out.
+				console.debug("page");
+				rudderAnalytics.page(
+					type,
+					pageName,
+					properties,
+					() => {
+						console.debug("Page event successfully recorded");
+					}
+				);
+			},
+		};
+		return rsClientSideEvents;
+	}).catch(err => {
+		console.error("ERROR: Could not load the Rudderstack SDK", err);
+		return mockAnalyticsObject;
 	});
-
-	const rudderstackClientSideEvents: RudderstackClientSideEvents = {
-		identify: (userId, name, email, anonymousId) => {  // Anonymous Id is set in local storage as soon as a user lands on the webiste.
-			console.debug("identify");
-			rudderAnalytics.identify(
-				userId,
-				{ name: name, email: email },
-				{ anonymousId: anonymousId },
-				() => {
-					console.debug("identify event successfully recorded");
-				}
-			);
-		},
-		/**
-		 * @param {*} properties properties should be a dictionary of properties of the event. It must contain an "eventStatusFlag" which will define the status of a single event. If the flag is 1, event is successful and if it is 0, event is failed.
-		 */
-		track: (userId, event, properties, anonymousId) => {
-			console.debug("track");
-			properties["userId"] = userId;
-			properties["event_timestamp"] = new Date();
-			rudderAnalytics.track(
-				event,
-				properties,
-				{ anonymousId: anonymousId },
-				() => {
-					console.debug("Track event successfully recorded");
-				}
-			);
-		},
-		page: (type, pageName, properties) => {
-			//TODO: Rudderstack doesn't take `userId` or `anonymousId` as arguments to page method but then does it makes sense to use this method? Figure out.
-			console.debug("page");
-			rudderAnalytics.page(
-				type,
-				pageName,
-				properties,
-				() => {
-					console.debug("Page event successfully recorded");
-				}
-			);
-		},
-	};
 	return rudderstackClientSideEvents;
 }
 
