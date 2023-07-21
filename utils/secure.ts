d2eb45442922066624686f6f6ee457f1400ee75c
VladimirKalita <105916007+VladimirKalita@users.noreply.github.com>
2023-07-21 11:37:56 +0530

    Update secure.ts

--- utils/secure.ts
+++ utils/secure.ts
@@ -1,27 +1,42 @@
-import crypto from "crypto";
+import * as crypto from 'crypto';
 
-const key = 'JaNdRgUkXp2s5u8x/A?D(G+KbPeShVmY'; // Replace with your secret key
-export const encryptToken = (token: string | undefined ) => {
-    const algorithm = "aes-256-cbc";
-    const iv = crypto.randomBytes(16).toString('hex').substring(0,16); // Generate a random initialization vector
-  
-    const cipher = crypto.createCipheriv(algorithm, key, iv);
-    if(token == undefined) return "hello";
-    let encrypted = cipher.update(token, "utf8", "hex");
-    encrypted += cipher.final("hex");
-    console.log(encrypted);
-    return iv+encrypted;
-  };
-  export const decryptToken = (encryptedToken: string) => {
-    const algorithm = "aes-256-cbc";
-    
-  
-    const iv = encryptedToken.substring(0, 16);
-    const encryptedData = encryptedToken.substring(16);
-  
-    const decipher = crypto.createDecipheriv(algorithm, key, iv);
-    let decrypted = decipher.update(encryptedData, "hex", "utf8");
-    decrypted += decipher.final("utf8");
-    
-    return decrypted;
-  };
+
+// const key = crypto.randomBytes(32);
+
+
+export function encryptToken(data: string | undefined): string {
+  const algorithm = 'aes-256-cbc';
+  const iv = crypto.randomBytes(16);
+  const key = 'JaNdRgUkXp2s5u8x/A?D(G+KbPeShVmY'
+  const cipher = crypto.createCipheriv(algorithm, key, iv);
+  cipher.setAutoPadding(true);
+  if(data === undefined) return "null";
+  let encryptedData = cipher.update(data, 'utf8', 'hex');
+  encryptedData += cipher.final('hex');
+
+  const combinedData = `${iv.toString('hex')}:${encryptedData}`;
+  return combinedData;
+}
+
+
+export function decryptToken(combinedData: string): string {
+  const algorithm = 'aes-256-cbc';
+  const key = 'JaNdRgUkXp2s5u8x/A?D(G+KbPeShVmY'
+  const ivLength = 32; // Length of the IV in characters
+  const iv = combinedData.substring(0, ivLength);
+  const encryptedData = combinedData.substring(ivLength + 1); // Skip the ":" separator
+
+  const decipher = crypto.createDecipheriv(algorithm, key, Buffer.from(iv, 'hex'));
+  decipher.setAutoPadding(true);
+
+  let decryptedData = decipher.update(encryptedData, 'hex', 'utf8');
+  decryptedData += decipher.final('utf8');
+
+  return decryptedData;
+}
+
+// const dataToEncrypt = 'Hello, loloposdjvbvdivp';
+// const combinedData = encryptToken(dataToEncrypt);
+// const decryptedData = decryptToken(combinedData);
+
+// console.log('Decrypted Data:', decryptedData);
