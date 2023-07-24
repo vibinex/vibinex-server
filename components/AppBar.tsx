062efd2bfb704ed76af31db7e63f0f853e6c49ab
Avikalp Kumar Gupta <avikalpgupta@gmail.com>
2023-07-24 14:12:06 +0530

    Added Banner to AppBar and introduced the `offset` prop which decides if other content will be overlapped by the AppBar.

--- components/AppBar.tsx
+++ components/AppBar.tsx
@@ -1,19 +1,25 @@
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, useState } from "react";
+import Banner, { BannerHeightType } from "./Banner";
 
 const AppBar = (props: PropsWithChildren<{
 	position: 'absolute' | 'fixed' | 'relative' | 'static' | 'sticky',
+	offset: boolean,
 	className?: string,
 }>) => {
 	const heightClassNames = "h-14 landscape:h-12 md:h-16";
+	const [bannerHeight, setBannerHeight] = useState<BannerHeightType>();
+
 	return (
 		<>
-			<header className={props.position + " flex flex-col w-full box-border z-20 top-0 right-0 left-auto bg-primary-main shrink-0 print:absolute"}>
+			<header className={props.position + " w-full box-border z-20 top-0 right-0 left-auto shrink-0 print:absolute"}>
+				<Banner bannerHeight={bannerHeight} setBannerHeight={setBannerHeight} />
 				<div className={"relative flex items-center px-2 " + heightClassNames + " " + props.className}>
 					{props.children}
 				</div>
 			</header>
+			<div className={`${bannerHeight}`}></div>
 			{
-				((props.position === 'fixed') || ((props.position === 'absolute'))) ?
+				(props.offset && (props.position === 'fixed') || ((props.position === 'absolute'))) ?
 					(<div className={heightClassNames}></div>) :
 					null
 			}
--- views/MainAppBar.tsx
+++ views/MainAppBar.tsx
@@ -10,7 +10,7 @@ export default function MainAppBar() {
 	}, [])
 
 	return (
-		<AppBar position='fixed' className='w-full py-2 px-10 flex flex-row bg-primary-light border-b-2 border-b-secondary-dark'>
+		<AppBar position='fixed' offset={true} className='w-full py-2 px-10 flex flex-row bg-primary-light border-b-2 border-b-secondary-dark'>
 			<a href="/" className="mr-16 h-full overflow-clip flex flex-row items-center">
 				<h1 className='font-bold text-3xl sm:text-4xl'>
 					Vibinex
