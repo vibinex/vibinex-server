1cb98761635463aa79683c9ec91346010885a208
Avikalp Kumar Gupta <avikalpgupta@gmail.com>
2023-07-24 19:13:50 +0530

    AppBar: fixed the styling issues. Now offset works perfectly without restricting the height of AppBar

--- components/AppBar.tsx
+++ components/AppBar.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren, useState } from "react";
+import { PropsWithChildren, useEffect, useRef, useState } from "react";
 import Banner, { BannerHeightType } from "./Banner";
 
 const AppBar = (props: PropsWithChildren<{
@@ -7,23 +7,27 @@ const AppBar = (props: PropsWithChildren<{
 	className?: string,
 	backdropClassName?: string,
 }>) => {
-	const heightClassNames = "h-14 landscape:h-12 md:h-16";
+	const appbarRef = useRef<HTMLDivElement>(null);
+	const offsetRef = useRef<HTMLDivElement>(null);
 	const [bannerHeight, setBannerHeight] = useState<BannerHeightType>();
+
+	useEffect(() => {
+		if (props.offset && appbarRef.current && offsetRef.current) {
+			offsetRef.current.style.height = `${appbarRef.current.clientHeight}px`;
+		}
+	})
 
 	return (
 		<>
 			<header className={props.position + " w-full box-border z-20 top-0 right-0 left-auto shrink-0 print:absolute border-b-secondary-dark border-b-2 " + props.backdropClassName}>
 				<Banner bannerHeight={bannerHeight} setBannerHeight={setBannerHeight} />
-				<div className={"relative flex items-center px-2 " + heightClassNames + " " + props.className}>
+				<div ref={appbarRef} className={"relative flex items-center px-2 " + props.className}>
 					{props.children}
 				</div>
 			</header>
 			<div className={`${bannerHeight}`}></div>
-			{
-				(props.offset && (props.position === 'fixed') || ((props.position === 'absolute'))) ?
-					(<div className={heightClassNames}></div>) :
-					null
-			}
+			{(props.offset && (props.position === 'fixed') || ((props.position === 'absolute'))) &&
+				<div ref={offsetRef} className='relative'></div>}
 		</>
 	)
 }
