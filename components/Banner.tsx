0d77d771e0caf305e4d54e1d5ffa52fd4734c970
doomsday4 <amankhilani.ajmer@gmail.com>
2023-07-25 20:42:04 +0530

    renamed isMobile variable

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -13,57 +13,6 @@ const Banner = ({ bannerHeight, setBannerHeight }: {
 	const chromeExtensionLink = "https://chrome.google.com/webstore/detail/vibinex/jafgelpkkkopeaefadkdjcmnicgpcncc";
 	const [bannerHTML, setBannerHTML] = useState((<></>));
 
-	const setBanner = (situation: BannerSituation) => {
-		switch (situation) {
-			case "not-installed":
-				setBannerHeight(() => {
-					const bannerHeight = 32;
-					setBannerHTML((<>
-						<p className='text-center text-xl w-fit sm:max-w-1/2 h-fit my-auto'>
-							Browser extension is not installed
-						</p>
-						<Button id="add-to-chrome-btn" variant="outlined" href={chromeExtensionLink} target="_blank" className='h-fit text-center p-3 sm:p-4 px-20 rounded-lg font-bold text-[20px] my-auto ml-4'>
-							<Image src={chromeLogo} alt="chrome extension logo" className={`w-10 inline mr-2 border border-white rounded-full`}></Image>
-							Download the extension
-						</Button>
-					</>))
-					return `h-${bannerHeight}`;
-				})
-				break;
-            case "incompatible-browser":
-                setBannerHeight(() => {
-                    const bannerHeight = 24;
-                    setBannerHTML((<>
-						<Image src={chromeLogo} alt="chrome extension logo" className={`w-12 inline mr-8 m-6 border border-white rounded-full`}></Image>
-						<p className='text-center font-bold text-xl w-fit sm:max-w-1/2 h-fit my-auto'>
-							Vibinex is only supported in Chromium browsers<br />
-							<span className='text-sm font-normal'>Google Chrome, Microsoft Edge, Opera, Chromium, Brave etc.</span>
-						</p>
-					</>))
-                    return `h-${bannerHeight}`;
-                })
-                break;                  
-			case "incompatible-device":
-				setBannerHeight(() => {
-					const bannerHeight = 12;
-					setBannerHTML((<>
-						<p className='text-center text-sm sm:text-xl w-fit sm:max-w-1/2 h-fit my-auto'>
-							Browser extensions are not supported on this device.
-						</p>
-					</>))
-					return `h-${bannerHeight}`;
-				})
-				break;
-			default:
-				// either user is not logged in, or Vibinex extension is already installed
-				setBannerHeight(() => {
-					setBannerHTML((<></>))
-					return undefined;
-				})
-				break;
-		}
-	}
-
 	useEffect(() => {
 		const setBanner = (situation: BannerSituation) => {
 			switch (situation) {
@@ -86,7 +35,7 @@ const Banner = ({ bannerHeight, setBannerHeight }: {
 					setBannerHeight(() => {
 						const bannerHeight = 24;
 						setBannerHTML((<>
-							<Image src={chromeLogo} alt="chrome extension logo" className={`inline m-6 mr-8 border border-white rounded-full w-${bannerHeight - 12}`}></Image>
+							<Image src={chromeLogo} alt="chrome extension logo" className={`w-12 inline mr-8 m-6 border border-white rounded-full`}></Image>
 							<p className='text-center font-bold text-xl w-fit sm:max-w-1/2 h-fit my-auto'>
 								Vibinex is only supported in Chromium browsers<br />
 								<span className='text-sm font-normal'>Google Chrome, Microsoft Edge, Opera, Chromium, Brave etc.</span>
@@ -94,7 +43,7 @@ const Banner = ({ bannerHeight, setBannerHeight }: {
 						</>))
 						return `h-${bannerHeight}`;
 					})
-					break;
+					break;                  
 				case "incompatible-device":
 					setBannerHeight(() => {
 						const bannerHeight = 12;
@@ -117,11 +66,11 @@ const Banner = ({ bannerHeight, setBannerHeight }: {
 		}
 
 		const determineSituation = (): BannerSituation => {
-            const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+            const isUnsupportedDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 			// currently, this is the best way to check if browser extensions are supported. Ref: https://stackoverflow.com/a/60927213/4677052
-			if ('chrome' in window && !isMobile) {
+			if ('chrome' in window && !isUnsupportedDevice) {
 				return null;
-			} else if (isMobile) {
+			} else if (isUnsupportedDevice) {
                 return "incompatible-device";
             } else {
 				return "incompatible-browser";
