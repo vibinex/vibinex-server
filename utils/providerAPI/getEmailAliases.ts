9a904bf864d66f276af696d61aab688b92b6be9b
Avikalp Kumar Gupta <avikalpgupta@gmail.com>
2023-08-30 17:54:37 +0530

    Removed unnecessary changes.

--- utils/providerAPI/getEmailAliases.ts
+++ utils/providerAPI/getEmailAliases.ts
@@ -25,69 +25,6 @@ type GitlabEmailObj = {
 
 type ProviderEmailObj = GithubEmailObj | BitbucketEmailObj | GitlabEmailObj;
 
-/**
- * This function facilitates auth handling for calling the GitHub/Bitbucket/GitLab APIs.
- * Note: Callback functions must return void since we want to run this function for all the different access_tokens available for this provider
- * @param provider The platform which hosts the account for the user and the repositories they use Vibinex with
- * @param session contains the auth_info object (with the access_token and the refresh_token)
- * @param apiEndPoint the target end-point
- * @param callback is the function that is executed in the case of success
- * @param errorCallback is the function that is executed in the case of failure
- */
-// export const callProviderAPI = (
-// 	provider: 'github' | 'bitbucket' | 'gitlab',
-// 	session: Session,
-// 	apiEndPoint: string,
-// 	callback: (value: AxiosResponse<any, any>) => void,
-// 	errorCallback: (reason: any) => void
-// ) => {
-// 	if (Object.keys(session.user.auth_info!).includes(provider)) {
-// 		for (const provider_auth_info of Object.values(session.user.auth_info![provider])) {
-// 			const access_key: string = provider_auth_info['access_token']; // handle expired access token with refresh token here
-// 			axios.get(baseURL + apiEndPoint, {
-// 				headers: {
-// 					'Accept': (provider === 'github') ? 'application/vnd.github+json' : 'application/json',
-// 					'Authorization': `Bearer ${access_key}`
-// 				}
-// 			})
-// 				.then(callback)
-// 				.catch(errorCallback)
-// 		}
-// 	} else {
-// 		console.warn(`${provider} provider not present`);
-// 	}
-// }
-
-// callProviderAPI('github', session, '/user/emails',
-// 	(response: { data: GithubEmailObj[] }) => {
-// 		const aliases = response.data.map((emailObj: GithubEmailObj) => emailObj.email);
-// 		updateUser(session.user.id!, { aliases: aliases }).catch(err => {
-// 			console.error(`[Profile] Could not update aliases for user (userId: ${session.user.id})`, err)
-// 		})
-// 	}, err => {
-// 		console.error(`[Profile] Error occurred while getting user emails from Github API. userId: ${session.user.id}, name: ${session.user.name}`, err);
-// 	});
-
-// callProviderAPI('bitbucket', session, '/user/emails',
-// 	(response: { data: { values: BitbucketEmailObj[] } }) => {
-// 		const aliases = response.data.values.map((emailObj: BitbucketEmailObj) => emailObj.email);
-// 		updateUser(session.user.id!, { aliases: aliases }).catch(err => {
-// 			console.error(`[Profile] Could not update aliases for user (userId: ${session.user.id})`, err)
-// 		})
-// 	},
-// 	err => {
-// 		console.error(`[Profile] Error occurred while getting user emails from Bitbucket API. userId: ${session.user.id}, name: ${session.user.name}`, err.message);
-// 	})
-
-// callProviderAPI('gitlab', session, '/user/emails',
-// 	(response: { data: GitlabEmailObj[] }) => {
-// 		const aliases = response.data.map((emailObj: GitlabEmailObj) => emailObj.email);
-// 		updateUser(session.user.id!, { aliases: aliases }).catch(err => {
-// 			console.error(`[Profile] Could not update aliases for user (userId: ${session.user.id})`, err)
-// 		})
-// 	}, err => {
-// 	})
-
 export const getEmailAliases = async (session: Session) => {
 	const allUserEmailsPromises = [];
 	for (const repoProvider of supportedProviders) {
