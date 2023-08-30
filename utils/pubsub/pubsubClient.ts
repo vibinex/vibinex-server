d9e46273cf995760ac45c7f49146bd40113677df
Tapish Rathore <tapish303@gmail.com>
2023-08-30 14:37:07 +0530

    Added pubsub message type

--- utils/pubsub/pubsubClient.ts
+++ utils/pubsub/pubsubClient.ts
@@ -1,10 +1,11 @@
 import { PubSub } from '@google-cloud/pubsub';
+import PubSubMessage from '../../types/PubSubMessage';
 
 // Set up authentication and initialize PubSub client
 const pubsub = new PubSub({ projectId: process.env.PROJECT_ID });
 
 
-export function publishMessage(topicName: string, data: any, msgtype: string) {
+export function publishMessage(topicName: string, data: PubSubMessage, msgtype: string) {
   const topic = pubsub.topic(topicName);
   const message = {
       data: Buffer.from(JSON.stringify(data)),
