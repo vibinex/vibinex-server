5bc2d60e5ae9388b7ba953ef56a097367de4bc49
Tapish Rathore <tapish303@gmail.com>
2023-09-02 12:15:23 +0530

    fix hunkjson and handler name

--- pages/api/hunks.ts
+++ pages/api/hunks.ts
@@ -1,11 +1,11 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { saveHunk } from '../../utils/db/relevance';
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const hunkjson = req.body;
-  console.info("Saving hunks...", hunkjson);
+const hunkHandler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const hunkJson = req.body;
+  console.info("Saving hunks...");
   try {
-    await saveHunk(hunkjson);
+    await saveHunk(hunkJson);
   } catch (error) {
     console.error('Error publishing message:', error);
     res.status(500).json({ error: 'Failed to publish message. Data must be in the form of a Buffer.' });
@@ -13,4 +13,4 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   res.status(200).send("Success");
 }
 
-export default handler;
+export default hunkHandler;
