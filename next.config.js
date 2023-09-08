f0eb961cab063b19d4df5f7eb481757fb137689b
Tapish Rathore <tapish303@gmail.com>
2023-09-08 12:47:22 +0530

    add comment for cors config

--- next.config.js
+++ next.config.js
@@ -53,7 +53,7 @@ const nextConfig = {
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, //TODO - narrow down this value
           {
             key: "Access-Control-Allow-Methods",
             value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
