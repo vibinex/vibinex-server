e3b142bdcc3fa92bbb0fac0ed5b13e46ff1e2d8a
Avikalp Kumar Gupta <avikalpgupta@gmail.com>
2023-09-15 17:30:35 +0530

    minor edit: added [mock-rs] prefix to the info logs of the mock rudderstack event functions

--- pages/api/events.ts
+++ pages/api/events.ts
@@ -2,8 +2,8 @@ import Analytics from "@rudderstack/rudder-sdk-node";
 import { apiObject } from "rudder-sdk-js";
 // we need the batch endpoint of the Rudder server you are running
 const client = (process.env.NODE_ENV === 'development') ? {
-	identify: (event_properties: object) => console.info(event_properties),
-	track: (event_properties: object) => console.info(event_properties)
+	identify: (event_properties: object) => console.info("[mock-rs] identify", event_properties),
+	track: (event_properties: object) => console.info("[mock-rs] track", event_properties)
 } : new Analytics(process.env.RUDDERSTACK_SERVER_WRITE_KEY!, { dataPlaneUrl: process.env.RUDDERSTACK_SERVER_DATA_PLANE_URL });
 
 const rudderStackEvents = {
