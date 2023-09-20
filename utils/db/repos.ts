1552810811b03a78960463185b7512fa998dceff
Avikalp Kumar Gupta <avikalpgupta@gmail.com>
2023-09-20 13:12:22 +0530

    In getRepos from database, each batch query runs concurrently with separated error handling.

--- utils/db/repos.ts
+++ utils/db/repos.ts
@@ -4,21 +4,30 @@ import { convert } from './converter';
 
 export const getRepos = async (allRepos: RepoIdentifier[]) => {
 	const batchSize = 50;
-	const allDbRepos = [];
+	const allDbReposPromises = [];
 	for (let index = 0; index < allRepos.length; index += batchSize) {
 		const allReposSubset = allRepos.slice(index, index + batchSize);
 		const allReposFormattedAsTuples = allReposSubset.map(repo => `(${convert(repo.repo_provider)}, ${convert(repo.repo_owner)}, ${convert(repo.repo_name)})`).join(',');
-		console.log('[getRepos] allReposFormatedAsTuples:', allReposSubset.length, allReposFormattedAsTuples.length, allReposFormattedAsTuples);
 		const repo_list_q = `SELECT *
 		FROM repos 
 		WHERE (repo_provider, repo_owner, repo_name) IN (${allReposFormattedAsTuples})
 		ORDER BY repo_provider, repo_owner, repo_name`;
-		const result: { rows: DbRepo[] } = await conn.query(repo_list_q).catch(err => {
+		const DbRepoSubsetPromise: Promise<{ rows: DbRepo[] }> = conn.query(repo_list_q).catch(err => {
 			console.error(`[getRepos] Error in getting repository-list from the database`, { pg_query: repo_list_q }, err);
-			throw new Error(err); // FIXME: handle this more elegantly
+			throw new Error(`Error in getting repository-list from the database. Batch: ${index}:${index + batchSize - 1}. Error: ${err.message}`);
 		});
-		allDbRepos.push(...result.rows);
+		allDbReposPromises.push(DbRepoSubsetPromise);
 	}
+
+	const allDbRepos = await Promise.allSettled(allDbReposPromises).then(results => {
+		const allDbRepoLists = results.map((result) => {
+			if (result.status !== 'fulfilled') {
+				return [];
+			}
+			return result.value.rows;
+		})
+		return allDbRepoLists.flat();
+	})
 
 	return allDbRepos;
 }
