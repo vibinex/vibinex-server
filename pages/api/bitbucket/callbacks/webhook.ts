405e8434ffe9279ee173c9be7959fe34627fe4eb
Tapish Rathore <tapish303@gmail.com>
2023-09-20 18:49:16 +0530

    fix try catch syntax in webhook

--- pages/api/bitbucket/callbacks/webhook.ts
+++ pages/api/bitbucket/callbacks/webhook.ts
@@ -8,38 +8,38 @@ const webhookHandler = async (req: NextApiRequest, res: NextApiResponse) => {
   const owner = jsonBody.repository.owner.username; 
   const provider = "bitbucket";
   const name = jsonBody.repository.name;
-  const topicName = await getTopicNameFromDB(owner, name, provider);
-
-  getRepoConfig({
-    repo_provider: provider,
-    repo_owner: owner,
-    repo_name: name
-  })
-  .then((repoConfig) => {
-    const data = {
-      repositoryProvider: 'bitbucket',
-      eventPayload: jsonBody,
-      repoConfig: repoConfig
-    };
-    
-    const msgType = 'webhook_callback';
-    
-    console.info("Received bitbucket webhook event for ", name);
-    console.debug(`data = ${JSON.stringify(jsonBody)}`)
-    console.debug(`topicname = ${topicName}`)
-    console.debug(`repoConfig = ${JSON.stringify(repoConfig)}`)
-    
-    return publishMessage(topicName, data, msgType);
-  })
-  .then(() => {
-    console.info("Sending message to pubsub for ", name);
-    res.status(200);
-    res.send("Success");
-  })
-  .catch((error) => {
+  const topicName = await getTopicNameFromDB(owner, name, provider).catch((error) => {
     console.error('Failed to get repoConfig from db :', error);
     res.status(500).json({ error: 'Internal Server Error' });
   });
+  const repoConfig = getRepoConfig({
+    repo_provider: provider,
+    repo_owner: owner,
+    repo_name: name
+  }).catch((error) => {
+    console.error('Failed to get repoConfig from db :', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  });
+  const data = {
+    repositoryProvider: 'bitbucket',
+    eventPayload: jsonBody,
+    repoConfig: repoConfig
+  };
+  
+  const msgType = 'webhook_callback';
+  
+  console.info("Received bitbucket webhook event for ", name);
+  console.debug(`data = ${JSON.stringify(jsonBody)}`)
+  console.debug(`topicname = ${topicName}`)
+  console.debug(`repoConfig = ${JSON.stringify(repoConfig)}`)
+  const topicName_str = topicName as string;
+  await publishMessage(topicName_str, data, msgType).catch((error) => {
+    console.error('Failed to get repoConfig from db :', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  });;
+  console.info("Sending message to pubsub for ", name);
+    res.status(200);
+    res.send("Success");
 }
 
 export default webhookHandler;
