94fd6a0264d560b6ac76553a2bd1b2ab954e6ef1
Tapish Rathore <tapish303@gmail.com>
2023-09-21 22:48:50 +0530

    fix catch and non-indent code and logs naming in webhook and hunk

--- pages/api/bitbucket/callbacks/webhook.ts
+++ pages/api/bitbucket/callbacks/webhook.ts
@@ -8,40 +8,50 @@ const webhookHandler = async (req: NextApiRequest, res: NextApiResponse) => {
   const owner = jsonBody.repository.owner.username; 
   const provider = "bitbucket";
   const name = jsonBody.repository.name;
-  const topicName = await getTopicNameFromDB(owner, name, provider).catch((error) => {
-    console.error('[bitbucket/webookHandler] Failed to get repoConfig from db:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-    return;
+  console.info("[webookHandler] Received bitbucket webhook event for ", name);
+  const topicName: string | null = await getTopicNameFromDB(owner, name, provider).catch((error) => {
+    console.error('[webookHandler] Failed to get topic name from db:', error);
+    return null;
   });
-  const repoConfig = await getRepoConfig({
+  if (!topicName) {
+    res.status(500)
+      .json({ error: 'Unable to get topic name from db' });
+    return;
+  }
+  const repoConfig: any | null = await getRepoConfig({
     repo_provider: provider,
     repo_owner: owner,
     repo_name: name
   }).catch((error) => {
-    console.error('Failed to get repoConfig from db :', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-    return;
+    console.error('[webookHandler] Failed to get repoConfig from db :', error);
+    return null;
   });
+  if (!repoConfig) {
+    res.status(500).json({ error: 'Unable to get repoConfig from db' });
+    return;
+  }
   const data = {
     repositoryProvider: 'bitbucket',
     eventPayload: jsonBody,
     repoConfig: repoConfig
   };
-  
   const msgType = 'webhook_callback';
-  
-  console.info("Received bitbucket webhook event for ", name);
-  console.debug(`data = ${JSON.stringify(jsonBody)}`)
-  console.debug(`topicname = ${topicName}`)
-  console.debug(`repoConfig = ${JSON.stringify(repoConfig)}`)
+  console.debug(`[webookHandler] data = ${JSON.stringify(jsonBody)}`)
+  console.debug(`[webookHandler] topicname = ${topicName}`)
+  console.debug(`[webookHandler] repoConfig = ${JSON.stringify(repoConfig)}`)
   const topicName_str = topicName as string;
-  await publishMessage(topicName_str, data, msgType).catch((error) => {
-    console.error('Failed to get repoConfig from db :', error);
+  const result: string | null = await publishMessage(topicName_str, data, msgType)
+  .catch((error) => {
+    console.error('[webookHandler] Failed to get repoConfig from db :', error);
+    return null;
+  });
+  if (result == null) {
     res.status(500).json({ error: 'Internal Server Error' });
     return;
-  });;
-  console.info("Sending message to pubsub for ", name);
+  }
+  console.info("[webookHandler] Sent message to pubsub for ", topicName, result);
   res.status(200).send("Success");
+  return;
 }
 
 export default webhookHandler;
--- pages/api/hunks.ts
+++ pages/api/hunks.ts
@@ -3,12 +3,16 @@ import { saveHunk } from '../../utils/db/relevance';
 
 const hunkHandler = async (req: NextApiRequest, res: NextApiResponse) => {
   const hunkJson = req.body;
-  console.info("Saving hunks...");
-  try {
-    await saveHunk(hunkJson);
-  } catch (error) {
-    console.error('Error publishing message:', error);
-    res.status(500).json({ error: 'Failed to publish message. Data must be in the form of a Buffer.' });
+  console.info("[hunkHandler] Saving hunks...");
+  const result: void | null = await saveHunk(hunkJson)
+  .catch((error) => {
+    console.error('[hunkHandler] Error publishing message:', error);
+    return null;
+  });
+  if (result == null) {
+    res.status(500)
+      .json({ error: 'Failed to publish message. Data must be in the form of a Buffer.' });
+    return;
   }
   res.status(200).send("Success");
 }
