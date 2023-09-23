e076ec1e1d0ef2e69631c1bf81395f707038a9a1
Tapish Rathore <tapish303@gmail.com>
2023-09-24 01:13:56 +0530

    revert shell script and remove PRIVATE_KEY from env var names

--- secrets_as_env.sh
+++ secrets_as_env.sh
@@ -3,9 +3,5 @@
 while IFS= read -r line
 do
     SECRET_NAME=$(echo $line | cut -d'=' -f1)
-    if [ "$SECRET_NAME" == "PRIVATE_KEY" ]; then
-        export $SECRET_NAME="$(cat /workspace/$SECRET_NAME.txt)"
-    else
-        export $SECRET_NAME=$(cat /workspace/$SECRET_NAME.txt)
-    fi
+    export $SECRET_NAME=$(cat /workspace/$SECRET_NAME.txt)
 done < env-staging-names.txt
