a5baa68fdbeec9572188e0193c6e2812de5bc83e
Tapish Rathore <tapish303@gmail.com>
2023-09-24 00:51:08 +0530

    add env-staging-names.txt

--- secrets_as_env.sh
+++ secrets_as_env.sh
@@ -4,4 +4,4 @@ while IFS= read -r line
 do
     SECRET_NAME=$(echo $line | cut -d'=' -f1)
     export $SECRET_NAME=$(cat /workspace/$SECRET_NAME.txt)
-done < env-staging.txt
+done < env-staging-names.txt
