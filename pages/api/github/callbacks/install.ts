5cdab3d487c942c8927e59df7d26f82432655bd3
Muskan Paliwal <muskan10112002@gmail.com>
2023-10-09 12:08:46 +0530

    changed code flow to reduce nesting

--- pages/api/github/callbacks/install.ts
+++ pages/api/github/callbacks/install.ts
@@ -3,25 +3,25 @@ import { publishMessage } from '../../../../utils/pubsub/pubsubClient';
 
 const installHandler = async (req: NextApiRequest, res: NextApiResponse) => {
 	const topicName = process.env.TOPIC_NAME;
-	if (topicName) {
-		const data = {
-			'repository_provider': 'github',
-			'installation_code': req.query.code
-		};
-		const msgType = 'install_callback';
-		console.info("Recieved installation code for github, published to ", topicName);
-		const result: string | null = await publishMessage(topicName, data, msgType)
-		.catch((error) => {
-			console.error('[install_callback] Failed to publish message:', error);
-			return null;
-		});
-		if (result == null) {
-			res.status(500).json({ error: 'Internal Server Error' });
-			return;
-		}
-	} else {
+	if (!topicName) {
 		console.error('TOPIC_NAME not set');
 		res.status(400).json({ error: 'Failed to publish message'});
+		return;
+	}
+	const data = {
+		'repository_provider': 'github',
+		'installation_code': req.query.code
+	};
+	const msgType = 'install_callback';
+	console.info("Recieved installation code for github, published to ", topicName);
+	const result: string | null = await publishMessage(topicName, data, msgType)
+	.catch((error) => {
+		console.error('[install_callback] Failed to publish message:', error);
+		return null;
+	});
+	if (result == null) {
+		res.status(500).json({ error: 'Internal Server Error' });
+		return;
 	}
 	res.redirect('/u');
 }
