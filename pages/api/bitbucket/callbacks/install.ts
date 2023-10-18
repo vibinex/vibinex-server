e3686709fd9b6b4ba49b770b3161a8dd38f6eea2
Avikalp Kumar Gupta <avikalpgupta@gmail.com>
2023-10-18 15:50:28 +0530

    ui: [install] Implemented never nesting in bitbucket install callback, improved error messages.

--- pages/api/bitbucket/callbacks/install.ts
+++ pages/api/bitbucket/callbacks/install.ts
@@ -4,24 +4,33 @@ import constructHtml from '../../../../utils/serverSideHTML';
 
 const installHandler = async (req: NextApiRequest, res: NextApiResponse) => {
 	const topicName = process.env.TOPIC_NAME;
-	if (topicName) {
-		const data = {
-			'repository_provider': 'bitbucket',
-			'installation_code': req.query.code
-		};
-		const msgType = 'install_callback';
-		console.info("Received installation code for bitbucket, published to ", topicName);
-		try {
-			res.write(constructHtml("Starting installation...<br><small>This may take some time. Please be patient.</small>"));
-			await publishMessage(topicName, data, msgType);
-		} catch (error) {
-			console.error('Error publishing message:', error);
-			res.status(500).send(constructHtml("ERROR: Failed to publish message. Data must be in the form of a Buffer.", "error"));
-		}
-	} else {
-		console.error('TOPIC_NAME not set');
-		res.status(500).send(constructHtml("ERROR: Failed to publish message.", "error"));
+	if (!topicName) {
+		console.error('[bitbucket/installHandler] TOPIC_NAME not set');
+		res.status(500).send(constructHtml("Internal Server Error", "error"));
+		return;
 	}
+	if (!req.query.code) {
+		console.error(`[bitbucket/installHandler] Installation code not provided for topic: ${topicName}`);
+		res.status(400).json(constructHtml("Bad Request: Bitbucket did not send a valid auth code", "error"));
+		return;
+	}
+	const data = {
+		'repository_provider': 'bitbucket',
+		'installation_code': req.query.code
+	};
+	const msgType = 'install_callback';
+	console.info("[bitbucket/installHandler] Received installation code for bitbucket, published to ", topicName);
+	res.write(constructHtml("Starting installation...<br><small>This may take some time. Please be patient.</small>"));
+	const result: string | null = await publishMessage(topicName, data, msgType)
+		.catch((error) => {
+			console.error(`[bitbucket/installHandler] Error publishing message for the topic: ${topicName}`, error);
+			return null;
+		});
+	if (result == null) {
+		res.status(500).send(constructHtml("Internal Server Error", "error"));
+		return;
+	}
+
 	res.write(
 		`<script>
 			location.href="/u"
