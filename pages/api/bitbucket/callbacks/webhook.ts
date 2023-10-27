817bedf8c1bb9c113bc8cbd3255a3a2281fe8710
Muskan Paliwal <muskan10112002@gmail.com>
2023-10-27 15:43:08 +0530

    added eventType in data object to send to pubsub

--- pages/api/bitbucket/callbacks/webhook.ts
+++ pages/api/bitbucket/callbacks/webhook.ts
@@ -4,53 +4,65 @@ import { getTopicNameFromDB } from '../../../../utils/db/relevance';
 import { getRepoConfig } from '../../../../utils/db/repos';
 
 const webhookHandler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const jsonBody = req.body;
-  const owner = jsonBody.repository.owner.username; 
-  const provider = "bitbucket";
-  const name = jsonBody.repository.name;
-  console.info("[webookHandler] Received bitbucket webhook event for ", name);
-  const topicName: string | null = await getTopicNameFromDB(owner, name, provider).catch((error) => {
-    console.error('[webhookHandler] Failed to get topic name from db:', error);
-    return null;
-  });
-  if (!topicName) {
-    res.status(500)
-      .json({ error: 'Unable to get topic name from db' });
-    return;
-  }
-  const repoConfig = await getRepoConfig({
-    repo_provider: provider,
-    repo_owner: owner,
-    repo_name: name
-  }).catch((error) => {
-    console.error('[webookHandler] Failed to get repoConfig from db :', error);
-    return null;
-  });
-  if (!repoConfig) {
-    res.status(500).json({ error: 'Unable to get repoConfig from db' });
-    return;
-  }
-  const data = {
-    repositoryProvider: 'bitbucket',
-    eventPayload: jsonBody,
-    repoConfig: repoConfig
-  };
-  const msgType = 'webhook_callback';
-  console.debug(`[webookHandler] data = ${JSON.stringify(jsonBody)}`)
-  console.debug(`[webookHandler] topicname = ${topicName}`)
-  console.debug(`[webookHandler] repoConfig = ${JSON.stringify(repoConfig)}`)
-  const topicName_str = topicName as string;
-  const result: string | null = await publishMessage(topicName_str, data, msgType)
-  .catch((error) => {
-    console.error('[webookHandler] Failed to get repoConfig from db :', error);
-    return null;
-  });
-  if (result == null) {
-    res.status(500).json({ error: 'Internal Server Error' });
-    return;
-  }
-  console.info("[webookHandler] Sent message to pubsub for ", topicName, result);
-  res.status(200).send("Success");
+	if (req.method !== 'POST') {
+		res.status(405).json({ error: 'Method Not Allowed' });
+		return;
+	}
+
+	const jsonBody = req.body;
+	const eventHeader = req.headers['x-event-key'];
+	const owner = jsonBody.repository.owner.username; 
+	const provider = "bitbucket";
+	const repo_name = jsonBody.repository.name;
+
+	// Verify the event type
+	if (eventHeader === 'pullrequest:approved' || eventHeader === 'pullrequest:created' || eventHeader === 'pullrequest:updated') {
+		console.info("[webookHandler] Received bitbucket webhook event for ", repo_name);
+		const topicName: string | null = await getTopicNameFromDB(owner, repo_name, provider).catch((error) => {
+			console.error('[webhookHandler] Failed to get topic name from db:', error);
+			return null;
+		});
+		if (!topicName) {
+			res.status(500)
+				.json({ error: 'Unable to get topic name from db' });
+			return;
+		}
+		const repoConfig = await getRepoConfig({
+			repo_provider: provider,
+			repo_owner: owner,
+			repo_name: repo_name
+		}).catch((error) => {
+			console.error('[webookHandler] Failed to get repoConfig from db :', error);
+			return null;
+		});
+		if (!repoConfig) {
+			res.status(500).json({ error: 'Unable to get repoConfig from db' });
+			return;
+		}
+		const data = {
+			repositoryProvider: 'bitbucket',
+			eventPayload: jsonBody,
+			repoConfig: repoConfig,
+			eventType: eventHeader
+		};
+		const msgType = 'webhook_callback';
+		console.debug(`[webookHandler] data = ${JSON.stringify(data)}`)
+		console.debug(`[webookHandler] topicname = ${topicName}`)
+		console.debug(`[webookHandler] repoConfig = ${JSON.stringify(repoConfig)}`)
+		const result: string | null = await publishMessage(topicName, data, msgType)
+		.catch((error) => {
+			console.error('[webookHandler] Failed to get repoConfig from db :', error);
+			return null;
+		});
+		if (result == null) {
+			res.status(500).json({ error: 'Internal Server Error' });
+			return;
+		}
+		console.info("[webookHandler] Sent message to pubsub for ", topicName, result);
+		res.status(200).send("Success");
+	} else {
+		res.status(400).json({error: "Invalid event type"});
+	}
 }
 
 export default webhookHandler;
