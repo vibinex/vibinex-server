ca659a5fa5d793c330db24b2eb774dd1a5bd96bc
Muskan Paliwal <muskan10112002@gmail.com>
2023-11-22 15:16:16 +0530

    written a function to create topicName in gcloud as well

--- utils/pubsub/pubsubClient.ts
+++ utils/pubsub/pubsubClient.ts
@@ -2,7 +2,7 @@ import { PubSub } from '@google-cloud/pubsub';
 import PubSubMessage from '../../types/PubSubMessage';
 
 // Set up authentication and initialize PubSub client
-const pubsub = new PubSub({ projectId: process.env.PROJECT_ID });
+const pubsub = new PubSub({ projectId: process.env.PROJECT_ID , keyFilename: process.env.PUB_SUB_JSON});
 
 
 export function publishMessage(topicName: string, data: PubSubMessage, msgType: string) {
@@ -16,3 +16,13 @@ export function publishMessage(topicName: string, data: PubSubMessage, msgType: 
   return topic.publishMessage(message);
 
 }
+
+export async function createTopicNameInGcloud(userId: string, topicName: string) {  
+  try {
+    const [topic] = await pubsub.createTopic(topicName);
+    console.log(`[createTopicNameInGcloud] Topic ${topic.name} created.`);
+    return topic;
+} catch (error) {
+    console.error('[createTopicNameInGcloud] Failed to create topic name in gcloud:', error);
+    return null;
+}}
