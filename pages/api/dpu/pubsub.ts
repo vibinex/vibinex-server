b4f66763ea04e5aa848aa83bcd9b30c6ea7052ee
Muskan Paliwal <muskan10112002@gmail.com>
2023-11-24 23:01:06 +0530

    minor fixes in messaging

--- pages/api/dpu/pubsub.ts
+++ pages/api/dpu/pubsub.ts
@@ -6,12 +6,11 @@ const pubsubHandler = async (req: NextApiRequest, res: NextApiResponse) => {
     console.info("[pubsubHandler] pub sub setup info in db...");
     const jsonBody = req.body;
     if (!jsonBody.user_id || !jsonBody.provider || !jsonBody.org_name) {
-        console.error("[pubsubHandler] Invalid request body, 'info' is missing or not an array");
+        console.error("[pubsubHandler] Invalid request body");
         res.status(400).json({"error": "Invalid request body"});
         return;
     }
     const topicName = await createTopicName(jsonBody.user_id, jsonBody.provider, jsonBody.org_name);
-    console.log("[pubsubhandler] topicName", topicName);
     if (!topicName){
         console.error(`[pubsubHanlder] error in creating topic name`);
         res.status(500).json({"error": "topicName creation error"});
@@ -23,7 +22,6 @@ const pubsubHandler = async (req: NextApiRequest, res: NextApiResponse) => {
         res.status(500).json({"error": "topic creation error in gcloud"});
         return;
     }
-    console.log("[pubsubHandler] gcloud topic: ", topic);
     await saveTopicNameInUsersTable(jsonBody.user_id, topicName);
     console.log("[pubsubHandler] topic name created successfully and saved in db: ", topicName);
     return res.status(200).json({"success": "topic name created and saved successfully"});
