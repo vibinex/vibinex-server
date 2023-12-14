badbea9cf5bf5a682afb9990007fc7425661dfec
Avikalp Kumar Gupta <avikalpgupta@gmail.com>
2023-12-15 00:58:58 +0530

    ui: added the product demo video to the landing page (right after the first fold)

---
+++ views/Demo.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import LiteYouTubeEmbed from "react-lite-youtube-embed";
+import "react-lite-youtube-embed/dist/LiteYouTubeEmbed.css";
+
+const ProductDemo = () => {
+	return (
+		<div id='features' className='w-full text-center py-12'>
+			<h2 className='font-bold text-[2rem]'>Vibinex  <span className='text-[2rem] text-primary-main font-bold'>Demo</span> for GitHub</h2>
+			<div className='w-full md:w-2/3 mt-8 m-auto'>
+				<LiteYouTubeEmbed id="Mennyfd5KNU" title="Vibinex Code Review Demo for GitHub" />
+			</div>
+		</div>
+	)
+}
+export default ProductDemo
