a82d8d15ee3cfcb8672b31f1427996c29b498066
Avikalp Kumar Gupta <avikalpgupta@gmail.com>
2024-01-07 11:40:45 +0530

    minor: fix the prefix in the logs of trigger.ts (from pubsubHandler to triggerHandler)

--- pages/api/dpu/trigger.ts
+++ pages/api/dpu/trigger.ts
@@ -4,16 +4,16 @@ import { CloudBuildStatus, createTopicNameInGcloud, triggerBuildUsingGcloudApi, 
 import { DbUser, getUserById } from '../../../utils/db/users';
 
 const triggerHandler = async (req: NextApiRequest, res: NextApiResponse) => {
-    console.info("[pubsubHandler] pub sub setup info in db...");
+    console.info("[triggerHandler] pub sub setup info in db...");
     const jsonBody = req.body;
     let topicName : string;
     if (!jsonBody.user_id) {
-        console.error("[pubsubHandler] Invalid request body");
+        console.error("[triggerHandler] Invalid request body");
         res.status(400).json({"error": "Invalid request body"});
         return;
     }
     const user_data: DbUser = await getUserById(jsonBody.user_id).catch(err => {
-        console.error(`[pubsubHandler] error in getting user data`, err);
+        console.error(`[triggerHandler] error in getting user data`, err);
         return;
     });
     if (!user_data) {
@@ -24,7 +24,7 @@ const triggerHandler = async (req: NextApiRequest, res: NextApiResponse) => {
         topicName = user_data.topic_name;       
     } else {
         const generated_topic = await createTopicName(jsonBody.user_id).catch(err => {
-            console.error(`[pubsubHandler] error in creating topic name`, err);
+            console.error(`[triggerHandler] error in creating topic name`, err);
             return;
         });
         if (!generated_topic) {
@@ -32,7 +32,7 @@ const triggerHandler = async (req: NextApiRequest, res: NextApiResponse) => {
             return;
         }
         const gcloudTopic = await createTopicNameInGcloud(generated_topic).catch(err => {
-            console.error(`[pubsubHandler] error in creating topic in google cloud`, err);
+            console.error(`[triggerHandler] error in creating topic in google cloud`, err);
             return;
         })
         if (!gcloudTopic) {
@@ -43,43 +43,43 @@ const triggerHandler = async (req: NextApiRequest, res: NextApiResponse) => {
         try {
             await saveTopicNameInUsersTable(jsonBody.user_id, topicName);
         } catch (error) {
-            console.error("[pubsubHandler] Unable to save topic name in db, ", error);
+            console.error("[triggerHandler] Unable to save topic name in db, ", error);
             return res.status(500).json({ "error": "Internal server error" });
         }
     }
-    console.info("[pubsubHandler] topic name created successfully and saved in db: ", topicName);
+    console.info("[triggerHandler] topic name created successfully and saved in db: ", topicName);
 
     //check if already a build exists in users table for the user, if yes? check status else continue
     
     const buildStatus: CloudBuildStatus = await triggerBuildUsingGcloudApi(jsonBody.user_id, topicName).catch(err => {
-        console.error(`[pubsubHandler] error in triggering build`, err);
+        console.error(`[triggerHandler] error in triggering build`, err);
         return { success: false, message: 'Unable to trigger build using GCloud API' };
     });
-    console.info("[pubsubHandler] build status: ", buildStatus);
+    console.info("[triggerHandler] build status: ", buildStatus);
     if (!buildStatus.success) {
-        console.error('[pubsubHandler] Error triggering build:', buildStatus.message);
+        console.error('[triggerHandler] Error triggering build:', buildStatus.message);
         return res.status(500).json({ "error": buildStatus.message, success: false });
     }
     const projectId: string | undefined = process.env.PROJECT_ID;
     const location: string | undefined = process.env.CLOUD_BUILD_LOCATION;
     if (!projectId || !location) {
-        console.error('[pubsubHandler] Environment variables for projectId and location must be set');
+        console.error('[triggerHandler] Environment variables for projectId and location must be set');
         res.status(400).json({"error": "env variables must be set", success: false});
         return;
     }
     const buildId = buildStatus.buildDetails?.id;
     if (!buildId) {
-        console.error('[pubsubHandler] No build ID found in buildDetails');
+        console.error('[triggerHandler] No build ID found in buildDetails');
         return res.status(500).json({ error: 'No build ID found', success: false });
     }
     const finalBuildStatus = await pollBuildStatus(projectId, location, buildId).catch(err => {
-        console.error(`[pubsubHandler] error in polling build status`, err);
+        console.error(`[triggerHandler] error in polling build status`, err);
         return 'ERROR';
     });
     if (finalBuildStatus === 'SUCCESS') {
         return res.status(200).json({ message: 'Build completed successfully', success: true, install_id: topicName });
     } else {
-        console.error(`[pubsubHandler] Build failed with status: ${finalBuildStatus}`);
+        console.error(`[triggerHandler] Build failed with status: ${finalBuildStatus}`);
         return res.status(500).json({ error: `Build failed with status: ${finalBuildStatus}`, success: false });
     }
 }
