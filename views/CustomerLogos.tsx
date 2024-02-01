2049d0273497a413fc7b23777cbe8d261ab7d1f1
Avikalp Kumar Gupta <avikalpgupta@gmail.com>
2024-02-01 13:48:30 +0530

    landing: added coverforce logo, and modified styling to suit 6 company logos

--- views/CustomerLogos.tsx
+++ views/CustomerLogos.tsx
@@ -5,10 +5,12 @@ import SupplyNoteLogo from '../public/SupplyNote-logo.png'
 import VyapLogo from '../public/Vyap-Logo.png'
 import BlitzLogo from '../public/blitz_logo_black.png'
 import AbleJobsLogo from '../public/Able-Jobs-logo.png'
+import CoverForceLogo from '../public/CoverForce Logo.png'
 
 const data = [
 	{ logo: SupplyNoteLogo, heading: 'SupplyNote' },
 	{ logo: AbleJobsLogo, heading: 'AbleJobs' },
+	{ logo: CoverForceLogo, heading: 'CoverForce', customClass: 'py-2' },
 	{ logo: BlitzLogo, heading: 'Blitz' },
 	{ logo: BillNoteLogo, heading: 'BillNote' },
 	{ logo: VyapLogo, heading: 'Vyap' },
@@ -24,14 +26,14 @@ const Customers = () => {
 				</span>
 				{' teams'}
 			</h2>
-			<div className='w-[90%] lg:w-2/3 mt-8 flex flex-row justify-center items-center place-content-between mx-auto flex-wrap lg:flex-nowrap'>
+			<div className='w-[90%] lg:w-2/3 mt-8 flex flex-row justify-center items-center place-content-between mx-auto flex-wrap xl:flex-nowrap sm:gap-y-4'>
 				{data.map((item) => (
 					<Image
 						priority
 						src={item.logo}
 						alt={item.heading}
 						key={item.heading}
-						className='w-1/2 sm:w-1/5 lg:w-full h-20 sm:h-16 md:h-12 sm:mt-2 lg:ml-6 object-contain px-4 py-1'
+						className={`${item.customClass ?? ''} w-1/2 sm:w-1/3 xl:w-full h-20 sm:h-16 md:h-12 xl:ml-6 object-contain px-4 py-1`}
 					/>
 				))}
 			</div>
