4b5422080f1be61b360f2e31e7d074627187f26c
Muskan Paliwal <muskan10112002@gmail.com>
2024-02-12 15:36:42 +0530

    created folder `setup` in extension folder and changed file name to `github`

--- pages/api/extension/setup/githubs.ts
+++ pages/api/extension/setup/githubs.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { getGithubReposFromDbForUserId } from '../../../utils/db/setup';
+import { getGithubReposFromDbForUserId } from '../../../../utils/db/setup';
 
 interface ErrorResponse {
 	error: string;
