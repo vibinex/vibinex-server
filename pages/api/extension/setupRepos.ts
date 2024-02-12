a29073c5617e0a5f9f312ee30156a1f177fd23fa
Muskan Paliwal <muskan10112002@gmail.com>
2024-02-12 15:16:29 +0530

    api that return setup github repo_names for a user, to be used by chrome-extension

---
+++ pages/api/extension/setupRepos.ts
@@ -0,0 +1,29 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { getGithubReposFromDbForUserId } from '../../../utils/db/setup';
+
+interface ErrorResponse {
+	error: string;
+	message: string;
+}
+
+interface SuccessResponse {
+	repos: string[];
+}
+
+export default async function githubSetupRepos(req: NextApiRequest, res: NextApiResponse<ErrorResponse | SuccessResponse>) {
+	if (req.method !== 'POST') {
+		return res.status(405).json({ error: 'Method Not Allowed', message: 'Only POST requests are allowed' });
+	}
+	const { user_id, org } = req.body;
+	if (!user_id || !org) {
+		return res.status(400).json({ error: 'Bad Request', message: 'Both user_id and org are required in the request body' });
+	}
+	getGithubReposFromDbForUserId(user_id, org, 'github')
+	.then((repos: string[]) => {
+		res.status(200).json({ repos: repos });
+	})
+	.catch((error: Error) => {
+		console.error('Error fetching repositories from database:', error);
+		res.status(500).json({ error: 'Internal Server Error', message: 'An error occurred while fetching repositories from the database' });
+	});
+}
