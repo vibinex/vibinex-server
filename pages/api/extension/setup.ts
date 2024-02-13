74ecdcd3cc8911878d541cbd78acf425335cec8c
Muskan Paliwal <muskan10112002@gmail.com>
2024-02-13 20:52:09 +0530

    generic api across providers

--- pages/api/extension/setup.ts
+++ pages/api/extension/setup.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { getGithubReposFromDbForUserId } from '../../../../utils/db/setup';
+import { getSetupReposFromDbForUserId } from '../../../utils/db/setup';
 
-export default async function githubSetupRepos(req: NextApiRequest, res: NextApiResponse) {
+export default async function setupRepos(req: NextApiRequest, res: NextApiResponse) {
 	// For cors prefetch options request
 	if (req.method == "OPTIONS") {
 		res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Origin, Content-Type, Authorization");
@@ -9,21 +9,21 @@ export default async function githubSetupRepos(req: NextApiRequest, res: NextApi
 		return;
 	}
 	// For normal requests
-	console.info("[extension/setup/github] Getting setup repos info for ", req.body.org);
+	console.info("[extension/setup] Getting setup repos info for ", req.body.org);
 	
 	if (req.method !== 'POST') {
 		return res.status(405).json({ error: 'Method Not Allowed', message: 'Only POST requests are allowed' });
 	}
-	const { userId, org } = req.body;
-	if (!userId || !org) {
-		return res.status(400).json({ error: 'Bad Request', message: 'Both user_id and org are required in the request body' });
+	const { userId, org, provider } = req.body;
+	if (!userId || !org || !provider) {
+		return res.status(400).json({ error: 'Bad Request', message: 'All the three arguments user_id, org, and provider are required in the request body' });
 	}
-	getGithubReposFromDbForUserId(userId, org, 'github')
+	getSetupReposFromDbForUserId(userId, org, provider)
 	.then((repos: string[]) => {
 		res.status(200).json({ repos: repos });
 	})
 	.catch((error: Error) => {
-		console.error('Error fetching repositories from database for user_id: ' + userId + ' and org: ' + org, error);
+		console.error('Error fetching repositories from database for user_id: ' + userId + ' , org: ' + org + ' and provider: ' + provider, error);
 		res.status(500).json({ error: 'Internal Server Error', message: 'An error occurred while fetching repositories from the database' });
 	});
 }
