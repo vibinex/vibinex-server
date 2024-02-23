78438fbd4a5181c93864b9ab76d0926d33fa6102
Muskan Paliwal <muskan10112002@gmail.com>
2024-02-23 13:07:40 +0530

    changes in logging

--- utils/db/setup.ts
+++ utils/db/setup.ts
@@ -1,13 +1,13 @@
 import conn from '.';
 
 export const getSetupReposFromDbForOwner = async (owner: string, provider: string): Promise<string[]> => {
-	console.log(`[getSetupReposFromDbForOrg] Getting setup repos from db for org ${owner} and provider ${provider}`);
+	console.log(`[getSetupReposFromDbForOwner] Getting setup repos from db for owner ${owner} and provider ${provider}`);
 	const query = `SELECT repos.repo_name
         FROM repos
         WHERE repos.repo_owner = $1 AND repos.repo_provider = $2;
     `;
 	const result = await conn.query(query, [owner, provider]).catch(err => {
-		console.error(`[getSetupReposFromDbForOrg] Could not get the github repos for org ${owner} and provider ${provider}`, { pg_query: query }, err);
+		console.error(`[getSetupReposFromDbForOwner] Could not get the github repos for owner ${owner} and provider ${provider}`, { pg_query: query }, err);
 		throw new Error("Error in running the query on the database" + err.message);
 	});
 	if (result.rows.length === 0) {
