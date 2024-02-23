64b66b7e55967d3beabb9a6ed4d4fd17034e7623
Muskan Paliwal <muskan10112002@gmail.com>
2024-02-23 10:55:47 +0530

    added a todo to find out a way of recognising active repos

--- utils/db/setup.ts
+++ utils/db/setup.ts
@@ -4,8 +4,8 @@ export const getSetupReposFromDbForOrg = async (org: string, provider: string): 
 	console.log(`[getSetupReposFromDbForOrg] Getting setup repos from db for org ${org} and provider ${provider}`);
 	const query = `SELECT repos.repo_name
         FROM repos
-        WHERE repos.repo_owner = $1 AND repos.repo_provider = $2;
-    `;
+        WHERE repos.repo_owner = $1 AND repos.repo_provider = $2; 
+    `; // TODO: Yet to handle use-case of recognising active repos. Right now, we only use this as a way to check if DPU is set and working on a repo
 	const result = await conn.query(query, [org, provider]).catch(err => {
 		console.error(`[getSetupReposFromDbForOrg] Could not get the github repos for org ${org} and provider ${provider}`, { pg_query: query }, err);
 		throw new Error("Error in running the query on the database" + err.message);
