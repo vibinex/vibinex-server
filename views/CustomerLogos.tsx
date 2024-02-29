d3c863fe0168d9ba1a3a8f567f506355a0f30b87
Avikalp Kumar Gupta <avikalpgupta@gmail.com>
2024-02-29 23:26:35 +0530

    ui: landing page: Customer logos now move in marquee across the screen. This can now handle any number of customer logos

--- views/CustomerLogos.tsx
+++ views/CustomerLogos.tsx
@@ -1,16 +1,19 @@
-import React from 'react'
+import AutoScroll from 'embla-carousel-auto-scroll'
 import Image from 'next/image'
+import CarouselWrapper from '../components/Carousal'
 import BillNoteLogo from '../public/BillNote-F.jpg'
+import CoverForceLogo from '../public/CoverForce-Logo.png'
 import SupplyNoteLogo from '../public/SupplyNote-logo.png'
 import VyapLogo from '../public/Vyap-Logo.png'
-import BlitzLogo from '../public/blitz_logo_black.png'
 import AbleJobsLogo from '../public/able_logo.png'
-import CoverForceLogo from '../public/CoverForce-Logo.png'
+import BlanceLogo from '../public/blance-full-logo-new.png'
+import BlitzLogo from '../public/blitz_logo_black.png'
 
 const data = [
 	{ logo: SupplyNoteLogo, heading: 'SupplyNote' },
 	{ logo: AbleJobsLogo, heading: 'Able' },
 	{ logo: CoverForceLogo, heading: 'CoverForce', customClass: 'py-2' },
+	{ logo: BlanceLogo, heading: 'Blance' },
 	{ logo: BlitzLogo, heading: 'Blitz' },
 	{ logo: BillNoteLogo, heading: 'BillNote' },
 	{ logo: VyapLogo, heading: 'Vyap' },
@@ -26,16 +29,18 @@ const Customers = () => {
 				</span>
 				{' teams'}
 			</h2>
-			<div className='w-[90%] lg:w-2/3 mt-8 flex flex-row justify-center items-center place-content-between mx-auto flex-wrap xl:flex-nowrap sm:gap-y-4'>
+			<div className='w-full mt-8 flex flex-row justify-center items-center place-content-between mx-auto flex-wrap xl:flex-nowrap sm:gap-y-4'>
+				<CarouselWrapper opts={{ loop: true }} plugins={[AutoScroll({ playOnInit: true })]} showControls={false} itemClassNames="basis-1/2 sm:basis-1/3 md:basis-1/4 xl:basis-1/6 shrink min-w-fit">
 				{data.map((item) => (
 					<Image
 						priority
 						src={item.logo}
 						alt={item.heading}
 						key={item.heading}
-						className={`${item.customClass ?? ''} w-1/2 sm:w-1/3 xl:w-full h-20 sm:h-16 md:h-12 xl:ml-6 object-contain px-4 py-1`}
+						className={`${item.customClass ?? ''} h-16 md:h-12 ml-6 object-contain px-4 py-4 sm:py-2 xl:py-1 w-fit`}
 					/>
 				))}
+				</CarouselWrapper>
 			</div>
 		</div>
 	)
