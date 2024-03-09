029581769a36b9408077a8bf7e2a36e6424503c6
Tapish Rathore <tapish@vibinex.com>
2024-03-09 19:03:09 +0530

    getRepoList api added

---
+++ pages/api/docs/getRepoList.ts
@@ -0,0 +1,19 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { getServerSession } from 'next-auth/next';
+import { getUserRepositories } from "../../../utils/providerAPI/getUserRepositories";
+import { authOptions } from '../auth/[...nextauth]';
+import { RepoIdentifier } from '../../../types/repository';
+
+const getUserRepositoriesHandler = async (req: NextApiRequest, res: NextApiResponse) => {
+    const session = await getServerSession(req, res, authOptions);
+	if (!session) {
+		return res.status(401).json({ error: 'Unauthenticated' });
+	}
+    const repoList: RepoIdentifier[] = await getUserRepositories(session);
+	if (repoList.length == 0) {
+		return res.status(500).json({error: 'Unable to get repo list'});
+	}
+	return res.status(200).json({repoList: repoList});
+}
+
+export default getUserRepositoriesHandler;
