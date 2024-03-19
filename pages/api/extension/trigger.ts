653746c46f9c40bade5139961042d3aea9d65068
Tapish Rathore <tapish@vibinex.com>
2024-03-19 16:18:44 +0530

    add a function to prepare body for trigger

--- pages/api/extension/trigger.ts
+++ pages/api/extension/trigger.ts
@@ -45,7 +45,7 @@ async function triggerDPU(url: string, userEmail: string) {
     // get repo config
     const repoConfig = await getRepoConfig(repoProvider, repoName, repoOwner, userId);
     // prepare body
-    
+    const triggerBody = prepareBody(repoProvider, repoOwner, repoName, prNumber, repoConfig);
     // get topic id
     // publish
     throw new Error('Function not implemented.');
@@ -89,4 +89,12 @@ function parseURL(url: string) {
     };
 }
 
-
+function prepareBody(repoProvider: string, repoOwner: string, repoName: string, prNumber: string, repoConfig: any) {
+    return {
+        repo_provider: repoProvider,
+        repo_owner: repoOwner,
+        repo_name: repoName,
+        pr_number: prNumber,
+        repo_config: repoConfig,
+    }
+}
