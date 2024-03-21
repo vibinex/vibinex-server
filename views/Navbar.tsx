cd23450d50c4d89f1f8d117c2955e67f64c43027
Avikalp Kumar Gupta <avikalpgupta@gmail.com>
2024-03-21 09:43:32 +0530

    ui: landing: fix responsiveness issue in Hero - but now Hero doesn't always occupy the entire landing fold

--- views/Navbar.tsx
+++ views/Navbar.tsx
@@ -81,7 +81,7 @@ const Navbar = (props: { transparent: boolean }) => {
 
 	return (
 		<AppBar position='fixed' className='mx-auto p-4 justify-between items-center max-w-7xl'
-			backdropClassName={'ease-in duration-300' + (scrollDown || !props.transparent ? ' bg-primary-light text-secondary-dark' : ' bg-transparent text-primary-light')}
+			backdropClassName={'ease-in duration-300' + (scrollDown || !props.transparent ? ' bg-primary-light text-secondary-dark' : ' bg-black text-primary-light')}
 		>
 			<Link href='/' className='flex items-center'>
 				<Image src={VibinexDarkLogo} alt="Vibinex logo" className="inline w-10 mr-2" priority></Image>
