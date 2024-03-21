f380d5b1f1a9f5d7127f0a0fe4f1082cf1d1c131
Avikalp Kumar Gupta <avikalpgupta@gmail.com>
2024-03-21 16:30:46 +0530

    api: eventing: add rs track calls for all types of the relevant API call

--- pages/api/extension/relevant.ts
+++ pages/api/extension/relevant.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import { getReviewData, getFileData, getHunkData, HunkInfo } from '../../../utils/db/relevance'
 import { getToken } from 'next-auth/jwt'
 import { getUserEmails } from '../../../utils/db/users'
+import rudderStackEvents from '../events'
 
 const getUser = async (req: NextApiRequest, res: NextApiResponse) => {
 	const user = await getToken({ req: req }).catch((err) => {
@@ -19,14 +20,15 @@ const relevantHandler = async (req: NextApiRequest, res: NextApiResponse) => {
 		return;
 	}
 	// For normal requests
+	if (!("repo_provider" in req.body) ||
+		!("repo_owner" in req.body) ||
+		!("repo_name" in req.body) ||
+		!("user_id" in req.body)) {
+		res.status(401).json({ error: 'Invalid request body' });
+	}
 	console.info("[extension/relevant] Getting relevant info for ", req.body.repo_name);
 	const user_emails = await getUser(req, res);
 	const { type } = req.query;
-	if (!("repo_provider" in req.body) ||
-		!("repo_owner" in req.body) ||
-		!("repo_name" in req.body)) {
-		res.status(401).json({ error: 'Invalid request body' });
-	}
 	let formattedData;
 	if (type === 'review') {
 		const reviewDb = await getReviewData(req.body.repo_provider,
@@ -37,10 +39,20 @@ const relevantHandler = async (req: NextApiRequest, res: NextApiResponse) => {
 			console.error("[extension/relevant] Error getting review data", err);
 		});
 		if (!reviewDb) {
+			rudderStackEvents.track(req.body.user_id, "", 'chrome_extension_event', {
+				...req.body,
+				function: 'relevant_prs',
+				eventStatusFlag: 0
+			});
 			res.status(500).json({ error: 'Internal server error' });
 			return;
 		}
-		formattedData = await formatReviewResponse(reviewDb);
+		formattedData = formatReviewResponse(reviewDb);
+		rudderStackEvents.track(req.body.user_id, "", 'chrome_extension_event', {
+			...req.body,
+			function: 'relevant_prs',
+			eventStatusFlag: 1
+		});
 	} else if (type === 'file') {
 		if (!("pr_number" in req.body)) {
 			res.status(400).json({ error: 'Invalid request body' });
@@ -52,6 +64,12 @@ const relevantHandler = async (req: NextApiRequest, res: NextApiResponse) => {
 			req.body.pr_number,
 			user_emails);
 		formattedData = formatFileResponse(fileSet);
+		rudderStackEvents.track(req.body.user_id, "", 'chrome_extension_event', {
+			...req.body,
+			function: 'relevant_file',
+			resultLength: formattedData.files.length,
+			eventStatusFlag: 1
+		});
 	} else if (type === 'hunk') {
 		if (!("pr_number" in req.body)) {
 			res.status(400).json({ error: 'Invalid request body' });
@@ -66,10 +84,21 @@ const relevantHandler = async (req: NextApiRequest, res: NextApiResponse) => {
 			console.error("[extension/relevant] Error getting hunk data", err);
 		});
 		if (!hunkRes) {
+			rudderStackEvents.track(req.body.user_id, "", 'chrome_extension_event', {
+				...req.body,
+				function: 'relevant_hunks',
+				eventStatusFlag: 0
+			});
 			res.status(500).json({ error: 'Internal server error' });
 			return;
 		}
 		formattedData = formatHunkResponse(hunkRes);
+		rudderStackEvents.track(req.body.user_id, "", 'chrome_extension_event', {
+			...req.body,
+			function: 'relevant_hunks',
+			resultLength: formattedData.hunkinfo.length,
+			eventStatusFlag: 1
+		});
 	}
 	res.status(200).json(formattedData);
 }
