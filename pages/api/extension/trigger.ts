d6a5b9c751836cad9b6772a84570c9f70c73d5ac
Tapish Rathore <tapish@vibinex.com>
2024-03-22 10:29:41 +0530

    Fix log tags for trigger

--- pages/api/extension/trigger.ts
+++ pages/api/extension/trigger.ts
@@ -18,22 +18,22 @@ export default async function triggeHandler(req: NextApiRequest, res: NextApiRes
 		return res.status(405).json({ error: 'Method Not Allowed', message: 'Only POST requests are allowed' });
 	}
     const user = await getToken({ req: req }).catch((err) => {
-		console.error("[bitbucket/relevant] Error getting user token", err);
+		console.error("[extension/triggeHandler] Error getting user token", err);
 		return null;
 	});
     if (!user || !user.email) {
-        console.error("[bitbucket/relevant] Error getting user");
+        console.error("[extension/triggeHandler] Error getting user");
         return res.status(401).json({ error: 'Unauthenticated', message: 'Incorrect auth token in request' });
     }
 	const { url } = req.body;
 	if (!url) {
-        console.error("[bitbucket/relevant] Error parsing url");
+        console.error("[extension/triggeHandler] Error parsing url");
 		return res.status(400).json({ error: 'Bad Request', message: 'url is required in the request body' });
 	}
     
 	await triggerDPU(url, user.email)
 		.catch((error: Error) => {
-			console.error('[extension/setup] Error triggering dpu for: ' + user.id + ' and pr: ' + url, error);
+			console.error('[extension/triggeHandler] Error triggering dpu for: ' + user.id + ' and pr: ' + url, error);
 			res.status(500).json({ error: 'Internal Server Error', message: 'An error occurred while triggering dpu' });
 		});
     res.status(200);
