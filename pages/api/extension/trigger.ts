60d2bb4b60cdd4a8aae269b44ddd11d3ec88551a
Tapish Rathore <tapish@vibinex.com>
2024-03-23 12:19:08 +0530

    fix response returning

--- pages/api/extension/trigger.ts
+++ pages/api/extension/trigger.ts
@@ -15,28 +15,36 @@ export default async function triggeHandler(req: NextApiRequest, res: NextApiRes
 	console.info("[extension/triggeHandler] Triggering DPU for ", req.body.url);
 
 	if (req.method !== 'POST') {
-		return res.status(405).json({ error: 'Method Not Allowed', message: 'Only POST requests are allowed' });
-	}
+	    res.status(405).json({ error: 'Method Not Allowed', message: 'Only POST requests are allowed' });
+        return;
+    }
     const user = await getToken({ req: req }).catch((err) => {
 		console.error("[extension/triggeHandler] Error getting user token", err);
 		return null;
 	});
     if (!user || !user.email) {
         console.error("[extension/triggeHandler] Error getting user");
-        return res.status(401).json({ error: 'Unauthenticated', message: 'Incorrect auth token in request' });
+        res.status(401).json({ error: 'Unauthenticated', message: 'Incorrect auth token in request' });
+        return;
     }
 	const { url } = req.body;
 	if (!url) {
         console.error("[extension/triggeHandler] Error parsing url");
-		return res.status(400).json({ error: 'Bad Request', message: 'url is required in the request body' });
-	}
+		res.status(400).json({ error: 'Bad Request', message: 'url is required in the request body' });
+        return;
+    }
     
-	await triggerDPU(url, user.email)
+	const result = await triggerDPU(url, user.email)
 		.catch((error: Error) => {
 			console.error('[extension/triggeHandler] Error triggering dpu for: ' + user.id + ' and pr: ' + url, error);
-			res.status(500).json({ error: 'Internal Server Error', message: 'An error occurred while triggering dpu' });
+			return null;
 		});
-    res.status(200);
+    if (result == null) {
+        res.status(500).json({ error: 'Internal Server Error', message: 'An error occurred while triggering dpu' });
+        return;
+    }
+    res.status(200).json({"message": "DPU triggered successfully"});
+    return;
 }
 async function triggerDPU(url: string, userEmail: string) {
     // parse url for repo name, owner, pr, provider
@@ -49,11 +57,13 @@ async function triggerDPU(url: string, userEmail: string) {
     const triggerBody = prepareBody(repoProvider, repoOwner, repoName, prNumber, repoConfig);
     // get topic id
     // publish
+    console.info(`[extension/triggerDPU] Publishing message ${triggerBody} to ${topicName}`);
     await publishMessage(topicName, triggerBody, "manual_trigger")
 		.catch((error) => {
-			console.error(`[triggerDPU] Failed to publish message on ${topicName}:`, error);
+			console.error(`[extension/triggerDPU] Failed to publish message on ${topicName}:`, error);
             throw new Error('Unable to publish message');
 		});
+    return true;
 }
 
 function parseURL(url: string) {
