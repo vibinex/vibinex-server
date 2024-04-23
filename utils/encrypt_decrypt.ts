2f11593098927c709befda7229ab4631333987e0
MuskanPaliwal <muskan10112002@gmail.com>
2024-04-23 12:20:03 +0530

    be: added error handling in encrypt-decrypt fucntions

--- utils/encrypt_decrypt.ts
+++ utils/encrypt_decrypt.ts
@@ -5,23 +5,35 @@ export const generateSecretKey = (): string => {
 }
 
 export const encrypt = (secretKey: string, data: string): string => {
-    // Generate an initialization vector (IV) from the secret key
-    const iv = crypto.createHash('sha256').update(secretKey).digest('hex').slice(0, 16);
-    // Create an encryption cipher
-    const cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(secretKey), Buffer.from(iv, 'hex'));
-    let encryptedData: string = cipher.update(data, 'utf8', 'hex');
-    encryptedData += cipher.final('hex');
-
-    return encryptedData;
+    try {
+        // Generate an initialization vector (IV) from the secret key
+        const iv = crypto.randomBytes(16);
+        // Create an encryption cipher
+        const cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(secretKey, 'hex'), iv);
+        let encryptedData: string = cipher.update(data, 'utf8', 'hex');
+        encryptedData += cipher.final('hex');
+    
+        return iv.toString('hex') + ':' + encryptedData;
+    } catch (error) {
+        console.error('[encrypt] Error encrypting data:', error);
+        throw error;
+    }
 };
 
 export const decrypt = (secretKey: string, data: string): string => {
-    // Generate an initialization vector (IV) from the secret key
-    const iv = crypto.createHash('sha256').update(secretKey).digest('hex').slice(0, 16);
-    // Create a decryption cipher
-    const decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(secretKey), Buffer.from(iv, 'hex'));
-    let decryptedData: string = decipher.update(data, 'hex', 'utf8');
-    decryptedData += decipher.final('utf8');
-
-    return decryptedData;
+   try {
+       // Generate an initialization vector (IV) from the secret key
+        const parts = data.split(':');
+        const iv = Buffer.from(parts[0], 'hex');
+        const encrypted_text = Buffer.from(parts[1], 'hex');
+        // Create a decryption cipher
+        const decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(secretKey, 'hex'), iv);
+        let decryptedData: string = decipher.update(encrypted_text).toString('utf8');;
+        decryptedData += decipher.final();
+    
+        return decryptedData;
+    } catch (error) {
+       console.error('[decrypt] Error decrypting data:', error);
+       throw error;
+   }
 };
