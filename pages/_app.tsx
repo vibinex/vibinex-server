05c43640c4a9455fc8b3a397a369a0abb1bfc39c
Tapish Rathore <tapish@vibinex.com>
2024-04-27 12:16:36 +0530

    Remove redundant changes in middleware and app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import '../styles/globals.css'
-// import '../pages/blog/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import { SessionProvider } from 'next-auth/react'
