82dbcb44cc1174b48bbcfe2f76b66cca092bc31e
Tapish Rathore <tapish@vibinex.com>
2024-04-30 16:05:24 +0530

    Use interface and nullis operator - sonarcloud

--- components/blog/Media.tsx
+++ components/blog/Media.tsx
@@ -14,12 +14,12 @@ interface MediaProps {
   };
 }
 
-export default function Media({ data }: { data: MediaProps }) {
+const Media = ({data} : {data: MediaProps} ) => {
   const imgUrl = getStrapiMedia(data.file.data.attributes.url);
   return (
 	<div className="flex items-center justify-center mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
 	  <Image
-		src={imgUrl || ""}
+		src={imgUrl ?? ""}
 		alt={data.file.data.attributes.alternativeText || "none provided"}
 		className="object-cover w-full h-full rounded-lg overflow-hidden"
 		width={400}
@@ -28,3 +28,5 @@ export default function Media({ data }: { data: MediaProps }) {
 	</div>
   );
 }
+
+export default Media;
