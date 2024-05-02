4a27d03487a65cf5a94c428d916c2e0472a6c88b
Avikalp Kumar Gupta <avikalpgupta@gmail.com>
2024-05-02 10:51:57 +0530

    ui: ProviderLogo can now also just return the src without the entire component

--- components/ProviderLogo.tsx
+++ components/ProviderLogo.tsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 import type { RepoProvider } from "../utils/providerAPI";
 import React from "react";
 
+export const getProviderLogoSrc = (provider: RepoProvider, theme: "light" | "dark") => `/${provider}${theme === 'dark' ? "-dark" : ""}.svg`;
+
 const ProviderLogo: React.FC<{ provider: RepoProvider, theme: "light" | "dark", className?: string }> = ({ provider, theme, className }) => {
 	return (
 		<Image
 			loading="lazy"
 			height={24} width={24}
-			src={`/${provider}${theme === 'dark' ? "-dark" : ""}.svg`}
+			src={getProviderLogoSrc(provider, theme)}
 			alt={provider}
 			title={provider}
 			className={className}
