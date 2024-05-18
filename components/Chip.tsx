effe7ba429e01d9696371fa709f6bdd06491dc3e
Avikalp Kumar Gupta <avikalpgupta@gmail.com>
2024-05-18 21:02:55 +0530

    ui: Chip: add cross icon if the chip is deletable

--- components/Chip.tsx
+++ components/Chip.tsx
@@ -1,18 +1,22 @@
 "use client";
 
 import Image from 'next/image';
+import Button from './Button';
+import { RxCross1 } from "react-icons/rx";
 
 interface ChipProps {
 	avatar: string;
 	name: string;
 	disabled: boolean;
+	disabledText?: string;
 	className?: string;
+	onDelete?: () => void;
 }
 
-const Chip: React.FC<ChipProps> = ({ name, avatar, disabled = false, className }) => (
+const Chip: React.FC<ChipProps> = ({ name, avatar, disabled = false, disabledText = '', className, onDelete }) => (
 	<div
 		className={`flex min-w-fit items-center space-x-1 outline outline-1 outline-slate-500 rounded-full py-1 px-3 m-1 ${className} ${disabled ? 'opacity-50 bg-action-inactive' : ''}`}
-		title={disabled ? 'This auth has expired' : ''}
+		title={disabled ? disabledText : ''}
 	>
 		<Image
 			src={avatar}
@@ -20,6 +24,9 @@ const Chip: React.FC<ChipProps> = ({ name, avatar, disabled = false, className }
 			width={24} height={24} className="h-6 rounded-full"
 		/>
 		<span className="text-md">{name}</span>
+		{ onDelete && (<Button variant='text' onClick={onDelete} className='!p-0'>
+			<RxCross1 className='hover:text-red-600 cursor-pointer' />
+		</Button>)}
 	</div>
 );
 
