401cd91d2768abbf991ae9ac2237257583c19530
Avikalp Kumar Gupta <avikalpgupta@gmail.com>
2024-05-21 17:27:34 +0530

    ui: fix: the Loading state was never unset if the API call returned error. - Refactor GitAliasForm useEffect and add toast when API fails.

--- views/GitAliasForm.tsx
+++ views/GitAliasForm.tsx
@@ -2,9 +2,11 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import Button from "../components/Button";
 import GitAliasListItem from "../components/GitAliasListItem";
+import { useToast } from "../components/Toast/use-toast";
 import { AliasMap, AliasProviderMap } from "../types/AliasMap";
 
 const GitAliasForm: React.FC<{ expanded: boolean }> = ({ expanded }) => {
+	const { toast } = useToast();
 	const [gitAliasMap, setGitAliasMap] = useState<AliasProviderMap | null>(null);
 	const [loading, setLoading] = useState<boolean>(true);
 
@@ -20,31 +22,34 @@ const GitAliasForm: React.FC<{ expanded: boolean }> = ({ expanded }) => {
 
 	useEffect(() => {
 		const fetchData = async () => {
-			try {
-				const response = await axios.get<{ aliasProviderMap: AliasProviderMap }>(`/api/alias`, {
-					params: { expanded },
-					headers: { 'Cache-Control': 'no-cache' }
-				});
-				if (!response.data?.aliasProviderMap) {
-					throw new Error('Failed to fetch Git email aliases');
-				}
-				const aliasProviderMap = response.data.aliasProviderMap;
-				aliasProviderMap.providerMaps.sort((a: AliasMap, b: AliasMap) => {
-					// First show all aliases without handles, then sort alphabetically
-					const aHasHandles = a.handleMaps.flatMap(handleMap => handleMap.handles).length !== 0;
-					const bHasHandles = b.handleMaps.flatMap(handleMap => handleMap.handles).length !== 0;
-					if (aHasHandles !== bHasHandles) {
-						return (aHasHandles > bHasHandles) ? 1 : -1;
-					}
-					return a.alias.localeCompare(b.alias);
-				})
-				setGitAliasMap(aliasProviderMap);
-				setLoading(false);
-			} catch (error) {
-				console.error("Error fetching Git email aliases:", error);
+			const response = await axios.get<{ aliasProviderMap: AliasProviderMap }>(`/api/alias`, {
+				params: { expanded },
+				headers: { 'Cache-Control': 'no-cache' }
+			});
+			if (!response.data?.aliasProviderMap) {
+				throw new Error('Failed to fetch Git email aliases');
 			}
+			const aliasProviderMap = response.data.aliasProviderMap;
+			aliasProviderMap.providerMaps.sort((a: AliasMap, b: AliasMap) => {
+				// First show all aliases without handles, then sort alphabetically
+				const aHasHandles = a.handleMaps.flatMap(handleMap => handleMap.handles).length !== 0;
+				const bHasHandles = b.handleMaps.flatMap(handleMap => handleMap.handles).length !== 0;
+				if (aHasHandles !== bHasHandles) {
+					return (aHasHandles > bHasHandles) ? 1 : -1;
+				}
+				return a.alias.localeCompare(b.alias);
+			})
+			setGitAliasMap(aliasProviderMap);
 		};
-		fetchData();
+		fetchData().catch((error) => {
+			console.error("Error fetching Git email aliases:", error);
+			toast({
+				description: error?.message ?? 'Failed to fetch Git email aliases',
+				variant: "destructive",
+			});
+		}).finally(() => {
+			setLoading(false);
+		});
 	}, [expanded]);
 
 	if (loading) {
