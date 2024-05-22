d9813eadb06aa8144c4e317aa44f86072c3f2552
MuskanPaliwal <muskan10112002@gmail.com>
2024-05-22 16:10:18 +0530

    formatting

--- pages/api/docs/getAllRepos.ts
+++ pages/api/docs/getAllRepos.ts
@@ -9,7 +9,7 @@ const getUserRepositoriesHandler = async (req: NextApiRequest, res: NextApiRespo
 	const session = await getServerSession(req, res, authOptions);
 	if (!session) {
 		const eventProperties = { response_status: 401 };
-        rudderStackEvents.track("absent", "", 'docs-user-repos-handler', { type: 'HTTP-401', eventStatusFlag: 0, eventProperties });
+		rudderStackEvents.track("absent", "", 'docs-user-repos-handler', { type: 'HTTP-401', eventStatusFlag: 0, eventProperties });
 		return res.status(401).json({ error: 'Unauthenticated' });
 	}
 	const repoList: RepoIdentifier[] | null = await getUserRepositories(session).catch(err => {
@@ -18,16 +18,16 @@ const getUserRepositoriesHandler = async (req: NextApiRequest, res: NextApiRespo
 	})
 	if (!repoList) {
 		const eventProperties = { response_status: 500 };
-		rudderStackEvents.track(session?.user?.id?? "absent", "", 'docs-user-repos-handler', { type: 'HTTP-500', eventStatusFlag: 0, eventProperties });    
+		rudderStackEvents.track(session?.user?.id ?? "absent", "", 'docs-user-repos-handler', { type: 'HTTP-500', eventStatusFlag: 0, eventProperties });
 		return res.status(500).json({ error: 'Error getting repositories' });
 	}
 	if (repoList.length == 0) {
 		const eventProperties = { response_status: 204, result_length: repoList.length };
-		rudderStackEvents.track(session?.user?.id?? "absent", "", 'docs-user-repos-handler', { type: 'HTTP-204', eventStatusFlag: 1, eventProperties });    
+		rudderStackEvents.track(session?.user?.id ?? "absent", "", 'docs-user-repos-handler', { type: 'HTTP-204', eventStatusFlag: 1, eventProperties });
 		return res.status(204).json({ error: 'No repositories found' });
 	}
 	const eventProperties = { response_status: 200, result_length: repoList.length };
-	rudderStackEvents.track(session?.user?.id?? "absent", "", 'docs-user-repos-handler', { type: 'HTTP-200', eventStatusFlag: 1, eventProperties });    
+	rudderStackEvents.track(session?.user?.id ?? "absent", "", 'docs-user-repos-handler', { type: 'HTTP-200', eventStatusFlag: 1, eventProperties });
 	return res.status(200).json({ repoList: repoList });
 }
 
