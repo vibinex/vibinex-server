9bc71d71b2d4198a4060bce734de9d296018bceb
Tapish Rathore <tapishr@Nami>
2024-05-29 11:58:26 +0530

    Implement new save repo function and call it

--- pages/api/docs/getAllRepos.ts
+++ pages/api/docs/getAllRepos.ts
@@ -3,6 +3,9 @@ import { getServerSession } from 'next-auth/next';
 import { RepoIdentifier } from '../../../types/repository';
 import { getUserRepositories } from "../../../utils/providerAPI/getUserRepositories";
 import { authOptions } from '../auth/[...nextauth]';
+import { getAuthUserId } from '../../../utils/auth';
+import { DbUser, getUserById } from '../../../utils/db/users';
+import { saveRepoIdentifierToDb } from '../../../utils/db/repos';
 
 const getUserRepositoriesHandler = async (req: NextApiRequest, res: NextApiResponse) => {
 	const session = await getServerSession(req, res, authOptions);
@@ -10,7 +13,7 @@ const getUserRepositoriesHandler = async (req: NextApiRequest, res: NextApiRespo
 		return res.status(401).json({ error: 'Unauthenticated' });
 	}
 	const repoList: RepoIdentifier[] | null = await getUserRepositories(session).catch(err => {
-		console.error('Error getting repositories', err);
+		console.error('[getUserRepositoriesHandler] Error getting repositories', err);
 		return null;
 	})
 	if (!repoList) {
@@ -19,7 +22,18 @@ const getUserRepositoriesHandler = async (req: NextApiRequest, res: NextApiRespo
 	if (repoList.length == 0) {
 		return res.status(204).json({ error: 'No repositories found' });
 	}
-	return res.status(200).json({ repoList: repoList });
+	res.status(200).json({ repoList: repoList });
+	const userId = getAuthUserId(session);
+	const userData: DbUser | null = await getUserById(userId).catch((err) => {
+		console.error('[getUserRepositoriesHandler] Error in getting user db data', err);
+		return null;
+	});
+	if (!userData?.topic_name) {
+		console.error(`[getUserRepositoriesHandler] Error in getting user topic from db`);
+		return;
+	}
+	const topicName = userData.topic_name;
+	await saveRepoIdentifierToDb(repoList, topicName);
 }
 
 export default getUserRepositoriesHandler;
