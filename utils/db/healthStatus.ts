4418f4600d2b5a5016749c0510d1b73a0aef61df
Tapish Rathore <tapish@vibinex.com>
2024-06-11 12:08:34 +0530

    Implement dpu health get api

--- utils/db/healthStatus.ts
+++ utils/db/healthStatus.ts
@@ -13,4 +13,17 @@ export const saveHealthStatusToDB = async (healthStatus: string, ts: string, top
 		console.error(`[getGitEmailAliasesFromDB] Error getting aliases for user ${topicId}:`, err);
 		throw new Error("Error getting aliases from the database");
 	})
-} 
+}
+
+export const getHealthStatusFromDB = async (user_id: string) => {
+	const healthStatusQuery = `
+	SELECT health_status, health_ts
+	FROM users
+	WHERE users.id = $1
+	`;
+	const { rows } = await conn.query(healthStatusQuery, [user_id]).catch(err => {
+		console.error(`[getGitEmailAliasesFromDB] Error getting aliases for user ${user_id}:`, err);
+		throw new Error("Error getting aliases from the database");
+	})
+	return rows[0];
+}
