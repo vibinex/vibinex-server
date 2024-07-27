1b5a6da878965adf49b7326ded87e8f25638cc8d
Abhishek Soni <abhisoni1764@gmail.com>
2024-07-27 15:13:55 +0530

    add aboutus page

---
+++ pages/about-us/index.tsx
@@ -0,0 +1,11 @@
+import OurPrinciples from "../../components/aboutus/our-principles";
+
+const AboutUs = () => {
+      return (
+            <div className="h-screen p-10">
+                <OurPrinciples/>
+            </div>
+      );
+};
+
+export default AboutUs;
