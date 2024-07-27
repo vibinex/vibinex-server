047a77610aaf0f573d49ea26a57a0ce8e66504c1
Shantanu Gahtori <shantanugahtori4@gmail.com>
2024-07-28 05:27:51 +0530

    add team section background

--- components/aboutus/our-team/index.tsx
+++ components/aboutus/our-team/index.tsx
@@ -1,10 +1,27 @@
 import React from "react";
 import FlipCard from "./FlipCard";
 import { teamMembers } from "../../../utils/about";
+import Image from "next/image";
 
 const OurTeam = () => {
   return (
-    <div className="flex flex-col p-10 items-center">
+    <div className="flex flex-col items-center min-h-screen pt-20 p-10">
+      <div className="flex absolute justify-between w-[100vw] h-[100vh] overflow-hidden">
+        <Image
+          src="/AboutUsDesignEcllipseHalf.svg"
+          alt="aboutus"
+          height={500}
+          width={500}
+          className="absolute left-0 -top-[5%]"
+        />
+        <Image
+          src="/AboutUsDesignGradient.svg"
+          alt="aboutus"
+          height={500}
+          width={500}
+          className="absolute right-0 -top-[5%]"
+        />
+      </div>
       <div>
         <h1 className="font-medium text-5xl tracking-normal font-lato">
           Our <span className="text-[#6B4CFF]">Team</span>
