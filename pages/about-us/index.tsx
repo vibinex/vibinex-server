f3b703042ba6173f1030f1e6050e9ab6a0d91910
Shantanu Gahtori <shantanugahtori4@gmail.com>
2024-07-28 04:53:58 +0530

    update files

--- pages/about-us/index.tsx
+++ pages/about-us/index.tsx
@@ -2,6 +2,8 @@ import AdSec from "../../components/aboutus/ad-section";
 import OurInvestors from "../../components/aboutus/our-investors";
 import OurPrinciples from "../../components/aboutus/our-principles";
 import OurTeam from "../../components/aboutus/our-team";
+import ProductVision from "../../components/aboutus/product-vision";
+import PurposeOfCompany from "../../components/aboutus/purpose";
 import Footer from "../../components/Footer";
 import MainAppBar from "../../views/MainAppBar";
 
@@ -9,7 +11,10 @@ const AboutUs = () => {
   return (
     <div>
       <MainAppBar />
-      <div className="p-10">
+
+      <div className=" bg-black">
+        <PurposeOfCompany />
+        <ProductVision />
         <OurPrinciples />
         <OurTeam />
         <OurInvestors />
