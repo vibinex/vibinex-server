2be8337ad58df70a1599c2a475e155d62aed171e
Tapish Rathore <tapish@vibinex.com>
2024-08-27 21:12:59 +0530

    reverting changes for creating demo graph

--- pages/api/docs/getDpuHealth.ts
+++ pages/api/docs/getDpuHealth.ts
@@ -6,7 +6,7 @@ import { getHealthStatusFromDB } from "../../../utils/db/healthStatus";
 
 const getDpuHealth = async (req: NextApiRequest, res: NextApiResponse) => {
 	const session = await getServerSession(req, res, authOptions);
-	if (!session || !session.user.id) {
+	if (!session || !session.user?.id) {
 		const eventProperties = { response_status: 401 };
 		rudderStackEvents.track("absent", "", 'user-repos-from-providers', { type: 'HTTP-401', eventStatusFlag: 0, eventProperties });
 		return res.status(401).json({ error: 'Unauthenticated' });
@@ -15,5 +15,3 @@ const getDpuHealth = async (req: NextApiRequest, res: NextApiResponse) => {
     const healthStatus = await getHealthStatusFromDB(user_id);
     res.status(200).json({healthStatus: healthStatus.health_status, healthTs: healthStatus.timestamp});
 }
-
-export default getDpuHealth;
