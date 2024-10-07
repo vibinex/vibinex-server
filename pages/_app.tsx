f029b4cda07b5c314180405c8b2d66442233fc8d
Avikalp Kumar Gupta <avikalpgupta@gmail.com>
2024-10-07 12:33:08 +0530

    minor: fix the 'do not call script tag in next/head, use next/script instead' browser error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import Script from 'next/script';
 import { useEffect, useState } from 'react';
 import RudderContext from '../components/RudderContext';
 import { Toaster } from '../components/Toast/toaster';
@@ -37,21 +38,19 @@ export default function App({ Component, pageProps: { session, ...pageProps } }:
 					<title>Vibinex • Review PRs 10x Faster on GitHub</title>
 					<meta name="description" content="Makes it easy to navigate, understand and review code changes in GitHub pull requests by adding context and personalization." />
 					<meta name="keywords" content={keywords.join(", ")} />
-
-					{/* Microsoft Clarity Update */}
-					<script
-						dangerouslySetInnerHTML={{
-							__html: `
+				</Head>
+				{/* Microsoft Clarity Update */}
+				<Script
+					dangerouslySetInnerHTML={{
+						__html: `
 					(function(c,l,a,r,i,t,y){
 						c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
 						t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
 						y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
 					})(window, document, "clarity", "script", "${process.env.CLARITY_ID}");
 					`
-						}}
-					/>
-
-				</Head>
+					}}
+				/>
 				<Component {...pageProps} />
 				<Toaster />
 			</SessionProvider>
