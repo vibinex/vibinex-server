1ff60afb0f8cc5388fc7fbb932c4cae194ffeb08
Avikalp Kumar Gupta <avikalpgupta@gmail.com>
2024-10-08 07:05:38 -0700

    meta: clarity script modified to remove dangerouslySetInnerHTML - pasted code from clarity website

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -41,16 +41,18 @@ export default function App({ Component, pageProps: { session, ...pageProps } }:
 				</Head>
 				{/* Microsoft Clarity Update */}
 				<Script
-					dangerouslySetInnerHTML={{
-						__html: `
+					id="microsoft-clarity"
+					strategy="afterInteractive"
+					type='text/javascript'
+				>
+					{`
 					(function(c,l,a,r,i,t,y){
 						c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
 						t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
 						y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
 					})(window, document, "clarity", "script", "${process.env.CLARITY_ID}");
-					`
-					}}
-				/>
+					`}
+				</Script>
 				<Component {...pageProps} />
 				<Toaster />
 			</SessionProvider>
