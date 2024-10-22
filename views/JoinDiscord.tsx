123d12103c686192bea470874de0622e97c8e556
Avikalp Kumar Gupta <avikalpgupta@gmail.com>
2024-10-22 08:24:26 -0700

    ui: JoinDiscord: fix UI for wide screens. Reduce complexity by decreasing the number of layers of div tags.

--- views/JoinDiscord.tsx
+++ views/JoinDiscord.tsx
@@ -13,7 +13,7 @@ const JoinDiscord = () => {
 
 	React.useEffect(() => {
 		const anonymousId = getAndSetAnonymousIdFromLocalStorage()
-		
+
 		const handleJoinDiscord = () => {
 			rudderEventMethods?.track(getAuthUserId(session), "join discord", { type: "button", eventStatusFlag: 1, source: "landing page banner", name: getAuthUserName(session) }, anonymousId)
 		}
@@ -28,27 +28,17 @@ const JoinDiscord = () => {
 	}, [rudderEventMethods, session])
 
 	return (
-		<div id='joinDiscord' className='w-full text-center py-12  bg-black mb-[-5%]'>
+		<div id='joinDiscord' className='w-full text-center py-12 bg-black'>
 			<h2 className='font-bold text-[2rem] text-white'>Discord Community</h2>
 			<div className='w-[100%] mt-1 p-4 text-white'>
 				<p className='text-[1.2rem]'>
 					Connect with us for any help or support.<br />Join our Discord community.
 				</p>
 			</div>
-
-			{/* TODO: Instead of a forever link, use this: https://github.com/thesandlord/SlackEngine */}
-			<Link id='join-discord' href={'https://discord.gg/caVSraCvpk'} target='blank'>
-				<div className='flex justify-center items-center'>
-					<div className='bg-secondary m-auto w-[50%] sm:p-5 p-3 px-20 rounded-lg font-bold sm:text-[25px] text-[20px] mt-5' >
-						<div className='flex text-primary-light justify-center items-center'>
-							<h2 className='sm:text-[1.5rem] text-[1rem] mr-4'>Join Now</h2>
-							<BsDiscord size={20} />
-						</div>
-					</div>
-				</div>
+			<Link id='join-discord' href={'https://discord.gg/caVSraCvpk'} target='blank' className='bg-secondary m-auto w-[50%] sm:p-5 p-3 px-20 rounded-lg font-bold sm:text-[25px] text-[20px] mt-5 inline-block flex text-primary-light justify-center items-center'>
+				<h2 className='sm:text-[1.5rem] text-[1rem] mr-4'>Join Now</h2>
+				<BsDiscord size={20} />
 			</Link>
-
-
 		</div>
 	)
 }
